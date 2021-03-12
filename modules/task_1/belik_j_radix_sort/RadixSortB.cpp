9e235fc3d694286470a560858e5b9c849a075c6e
BelikJulia <ybelik2015@yandex.ru>
2021-03-12 12:59:28 +0300

    task1v1

--- modules/task_1/belik_j_radix_sort/RadixSortB.cpp
+++ modules/task_1/belik_j_radix_sort/RadixSortB.cpp
@@ -6,8 +6,7 @@
 #include "../../../modules/task_1/belik_j_radix_sort/RadixSortB.h"
 std::vector<double> RadixSort(std::vector<double> vec) {
     std::vector<double> res = vec;
-    for (size_t i = 0; i < 7; i++)
-    {
+    for (size_t i = 0; i < 7; i++) {
         unsigned char* bvec = reinterpret_cast<unsigned char*>(res.data());
         int countb[256] = { 0 };
         int offset[256] = { 0 };
