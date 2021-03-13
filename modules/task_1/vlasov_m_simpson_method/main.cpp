06d960fad048b29bbff5e469461867e507e5bdcf
vla5924 <vla5924@gmail.com>
2021-03-13 12:00:27 +0300

    Project init for task 1

---
+++ modules/task_1/vlasov_m_simpson_method/main.cpp
@@ -0,0 +1,4 @@
+// Copyright 2021 Vlasov Maksim
+#include <gtest/gtest.h>
+#include <vector>
+#include "./simpson_method.h"
---
+++ modules/task_1/vlasov_m_simpson_method/simpson_method.cpp
@@ -0,0 +1,3 @@
+// Copyright 2021 Vlasov Maksim
+
+#include "../../../modules/task_1/vlasov_m_simpson_method/simpson_method.h"
