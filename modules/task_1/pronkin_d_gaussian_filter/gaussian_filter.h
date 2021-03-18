da9f4d3b3e4b76a2de6337f0295516940f3fdc45
r01and3 <dimka.pronckin@gmail.com>
2021-03-18 18:13:01 +0300

    Fix 1.1

--- modules/task_1/pronkin_d_gaussian_filter/gaussian_filter.h
+++ modules/task_1/pronkin_d_gaussian_filter/gaussian_filter.h
@@ -1,6 +1,6 @@
 // Copyright 2021 Pronkin Dmitry
-#ifndef MODULES_TASK_1_PRONKIN_DMITRY_GAUSSIAN_FILTER_GAUSSIAN_FILTER_H_
-#define MODULES_TASK_1_PRONKIN_DMITRY_GAUSSIAN_FILTER_GAUSSIAN_FILTER_H_
+#ifndef MODULES_TASK_1_PRONKIN_D_GAUSSIAN_FILTER_GAUSSIAN_FILTER_H_
+#define MODULES_TASK_1_PRONKIN_D_GAUSSIAN_FILTER_GAUSSIAN_FILTER_H_
 
 #include <vector>
 #include <stdexcept>
@@ -15,4 +15,4 @@ const int KERNEL[] = {1, 2, 1};
 std::vector<double> getRandomImage(int height, int width);
 std::vector<double> seqGaussianFilter(const std::vector<double>& image, int height, int width);
 
-#endif  // MODULES_TASK_1_PRONKIN_DMITRY_GAUSSIAN_FILTER_GAUSSIAN_FILTER_H_
+#endif  // MODULES_TASK_1_PRONKIN_D_GAUSSIAN_FILTER_GAUSSIAN_FILTER_H_
