ccf48b87f410f36c1229c7305ed337846fa44b14
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2021-03-19 19:37:15 +0300

    Update labeling.h

--- modules/task_1/paranicheva_a_labeling/labeling.h
+++ modules/task_1/paranicheva_a_labeling/labeling.h
@@ -8,6 +8,6 @@
 std::vector<int> getRandomMatrix(int rows, int cols);
 std::pair<std::vector<int>, std::vector<int> > firstMark(std::vector<int>, int, int);
 std::vector<int> secondMark(std::vector<int>, int, int, std::vector<int>);
-std::vector<int> CLabeling(const std::vector<int>, int, int);
+std::vector<int> CLabeling(const std::vector<int>&, int, int);
 
 #endif  // MODULES_TASK_1_PARANICHEVA_A_LABELING_LABELING_H_
