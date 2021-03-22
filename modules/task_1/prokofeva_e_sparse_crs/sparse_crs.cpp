f481f86f44e0231a7b5022c81fe3f41924c89e12
Elizaveta Prokofeva <prokof.15@mail.ru>
2021-03-22 20:02:46 +0300

    v4

--- modules/task_1/prokofeva_e_sparse_crs/sparse_crs.cpp
+++ modules/task_1/prokofeva_e_sparse_crs/sparse_crs.cpp
@@ -29,27 +29,27 @@ crs_matrix create(int size, std::vector<double> mat) {
 
 crs_matrix transpose(crs_matrix matrix) {
     crs_matrix Tmatrix;
-    Tmatrix.N = matrix.N;
+    int N = Tmatrix.N = matrix.N;
     Tmatrix.row_index = std::vector<int>(static_cast<size_t>(matrix.N + 1));
     Tmatrix.values = std::vector<double>(static_cast<size_t>(matrix.values.size()));
     Tmatrix.cols = std::vector<int>(static_cast<size_t>(matrix.cols.size()));
-
-    for (size_t i = 0; i < matrix.values.size(); i++) {
-        Tmatrix.row_index[matrix.cols[i] + 1]++;
+    int size = matrix.values.size();
+    for (size_t i = 0; i < size; i++) {
+        int col = matrix.cols[i];
+        Tmatrix.row_index[col + 1]++;
     }
 
     double s = 0;
-    for (int i = 1; i <= matrix.N; i++) {
+    for (int i = 1; i <= N; i++) {
         int tmp = Tmatrix.row_index[i];
         Tmatrix.row_index[i] = s;
         s += tmp;
     }
 
-    for (int i = 0; i < matrix.N; i++) {
+    for (int i = 0; i < N; i++) {
         for (int j = matrix.row_index[i]; j < matrix.row_index[i + 1]; j++) {
-            int r_ind = matrix.cols[j];
-            Tmatrix.values[Tmatrix.row_index[r_ind + 1]] = matrix.values[j];
-            Tmatrix.cols[Tmatrix.row_index[r_ind + 1]] = i;
+            Tmatrix.values[Tmatrix.row_index[matrix.cols[j] + 1]] = matrix.values[j];
+            Tmatrix.cols[Tmatrix.row_index[matrix.cols[j] + 1]] = i;
             Tmatrix.row_index[matrix.cols[j] + 1]++;
         }
     }
@@ -58,10 +58,13 @@ crs_matrix transpose(crs_matrix matrix) {
 
 double scalar_mult(crs_matrix A, crs_matrix B, int i, int j) {
     double sum = 0.0;
-    for (int k = A.row_index[i]; k < A.row_index[i + 1]; k++) {
-        for (int l = B.row_index[j]; l < B.row_index[j + 1]; l++) {
-            if (A.cols[k] == B.cols[l]) {
-                
+    int a = A.row_index[i];
+    int a1 = A.row_index[i + 1];
+    int b = B.row_index[j];
+    int b1 = B.row_index[j + 1];
+    for (int k = a; k < a1; k++) {
+        for (int l = b; l < b1; l++) {
+            if (A.cols[k] == B.cols[l]) { 
                 sum += A.values[k] * B.values[l];
                 break;
             }
@@ -75,21 +78,20 @@ crs_matrix mult(crs_matrix A, crs_matrix B) {
     crs_matrix result;
     B = transpose(B);
     double sum;
-    int rowNZ;
+    int rowNZ = 0;
+    int tmp = 0;
 
-    result.row_index.push_back(0);
+    result.row_index.emplace_back(tmp);
     for (int i = 0; i < N; i++) {
-        rowNZ = 0;
         for (int j = 0; j < N; j++) {
-
             sum = scalar_mult(A, B, i, j);
             if (sum != 0) {
-                result.cols.push_back(j);
-                result.values.push_back(sum);
+                result.cols.emplace_back(j);
+                result.values.emplace_back(sum);
                 rowNZ++;
             }
         }
-        result.row_index.push_back(rowNZ + result.row_index[i]);
+        result.row_index.emplace_back(rowNZ + result.row_index[i]);
     }
     return result;
 }
