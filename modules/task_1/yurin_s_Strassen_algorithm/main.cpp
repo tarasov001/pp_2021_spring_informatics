888243c7eaf0a76db3090f8525d4d6734f15a983
Stanislav Yurin <s.yurin@standart1c.ru>
2021-03-22 21:04:37 +0300

    test count

--- modules/task_1/yurin_s_Strassen_algorithm/main.cpp
+++ modules/task_1/yurin_s_Strassen_algorithm/main.cpp
@@ -32,7 +32,7 @@ TEST(Strassen_algorithm, TEST1) {
 }
 
 TEST(Strassen_algorithm, TEST2) {
-    int rows_num = 8;
+    int rows_num = 4;
 
     std::vector<double> a(rows_num * rows_num);
     std::vector<double> b(rows_num * rows_num);
@@ -58,7 +58,7 @@ TEST(Strassen_algorithm, TEST2) {
 }
 
 TEST(Strassen_algorithm, TEST3) {
-    int rows_num = 32;
+    int rows_num = 8;
 
     std::vector<double> a(rows_num * rows_num);
     std::vector<double> b(rows_num * rows_num);
@@ -84,7 +84,7 @@ TEST(Strassen_algorithm, TEST3) {
 }
 
 TEST(Strassen_algorithm, TEST4) {
-    int rows_num = 64;
+    int rows_num = 16;
 
     std::vector<double> a(rows_num * rows_num);
     std::vector<double> b(rows_num * rows_num);
@@ -110,7 +110,7 @@ TEST(Strassen_algorithm, TEST4) {
 }
 
 TEST(Strassen_algorithm, TEST5) {
-    int rows_num = 128;
+    int rows_num = 32;
 
     std::vector<double> a(rows_num * rows_num);
     std::vector<double> b(rows_num * rows_num);
@@ -121,6 +121,58 @@ TEST(Strassen_algorithm, TEST5) {
 
     for (int i = 0; i < static_cast<int>(b.size()); i++)
         b[i] = getRandomNumber(-41, 300);
+
+    std::vector<double> c_strass(rows_num * rows_num);
+    std::vector<double> c_default(rows_num * rows_num);
+    std::vector<double>::iterator c_it = c_default.begin();
+
+    c_strass = StrassenMulti(a, b);
+
+    matrMulti(a, b, c_it, rows_num);
+
+    for (int i = 0; i < static_cast<int>(c_strass.size()); i++) {
+        ASSERT_LT(abs(c_strass[i] - c_default[i]), EPS);
+    }
+}
+
+TEST(Strassen_algorithm, TEST6) {
+    int rows_num = 64;
+
+    std::vector<double> a(rows_num * rows_num);
+    std::vector<double> b(rows_num * rows_num);
+
+
+    for (int i = 0; i < static_cast<int>(a.size()); i++)
+        a[i] = getRandomNumber(-50, 81);
+
+    for (int i = 0; i < static_cast<int>(b.size()); i++)
+        b[i] = getRandomNumber(-2, 321);
+
+    std::vector<double> c_strass(rows_num * rows_num);
+    std::vector<double> c_default(rows_num * rows_num);
+    std::vector<double>::iterator c_it = c_default.begin();
+
+    c_strass = StrassenMulti(a, b);
+
+    matrMulti(a, b, c_it, rows_num);
+
+    for (int i = 0; i < static_cast<int>(c_strass.size()); i++) {
+        ASSERT_LT(abs(c_strass[i] - c_default[i]), EPS);
+    }
+}
+
+TEST(Strassen_algorithm, TEST7) {
+    int rows_num = 128;
+
+    std::vector<double> a(rows_num * rows_num);
+    std::vector<double> b(rows_num * rows_num);
+
+
+    for (int i = 0; i < static_cast<int>(a.size()); i++)
+        a[i] = getRandomNumber(0, 11);
+
+    for (int i = 0; i < static_cast<int>(b.size()); i++)
+        b[i] = getRandomNumber(-15, 10);
 
     std::vector<double> c_strass(rows_num * rows_num);
     std::vector<double> c_default(rows_num * rows_num);
