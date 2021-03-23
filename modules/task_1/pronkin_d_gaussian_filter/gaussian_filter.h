21e177487d8119faf34cefbdb87abb9058e8a95c
r01and3 <dimka.pronckin@gmail.com>
2021-03-23 20:22:25 +0300

    Version 2.0

--- modules/task_1/pronkin_d_gaussian_filter/gaussian_filter.h
+++ modules/task_1/pronkin_d_gaussian_filter/gaussian_filter.h
@@ -7,12 +7,14 @@
 #include <random>
 #include <ctime>
 #include <limits>
+#include <cmath>
+
+#define M_PI 3.14159265358979323846
 
 const int KERNEL_SIZE = 3;
-const unsigned int KERNEL_NORM = 4;
-const int KERNEL[] = {1, 2, 1};
 
 std::vector<double> getRandomImage(int height, int width);
-std::vector<double> seqGaussianFilter(const std::vector<double>& image, int height, int width);
+std::vector<double> getGaussianKernel(double sigma);
+std::vector<double> seqGaussianFilter(const std::vector<double>& image, int height, int width, double sigma);
 
 #endif  // MODULES_TASK_1_PRONKIN_D_GAUSSIAN_FILTER_GAUSSIAN_FILTER_H_
