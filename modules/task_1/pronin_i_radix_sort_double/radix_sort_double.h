bdb4592a9e4d0f4f479a71f32912c3ec0f882c76
igor-pronin <gosha.pronin.2000@icloud.com>
2021-03-23 20:51:09 +0300

    version 1

--- modules/task_1/pronin_i_radix_sort_double/radix_sort_double.h
+++ modules/task_1/pronin_i_radix_sort_double/radix_sort_double.h
@@ -2,11 +2,11 @@
 #ifndef MODULES_TASK_1_PRONIN_I_RADIX_SORT_DOUBLE_RADIX_SORT_DOUBLE_H_
 #define MODULES_TASK_1_PRONIN_I_RADIX_SORT_DOUBLE_RADIX_SORT_DOUBLE_H_
 
-#include <iostream>  
-#include <random> 
+#include <iostream>
+#include <random>
 #include <ctime>
 void RandVec(double*, int, int, int);
 void SortBite(double*, double*, int, int);
 void SortAll(double*, double*, int);
 
-#endif  // MODULES_TASK_1_PRONIN_I_RADIX_SORT_DOUBLE_RADIX_SORT_DOUBLE_H_
+#endif // MODULES_TASK_1_PRONIN_I_RADIX_SORT_DOUBLE_RADIX_SORT_DOUBLE_H_
