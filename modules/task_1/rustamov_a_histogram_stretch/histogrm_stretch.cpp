92db60677d23115fbfcc876d8a5520451ccbb3db
RustamovAzer <43135865+RustamovAzer@users.noreply.github.com>
2021-03-24 17:28:10 +0300

    24.03.2021

--- modules/task_1/rustamov_a_histogram_stretch/histogrm_stretch.cpp
+++ modules/task_1/rustamov_a_histogram_stretch/histogrm_stretch.cpp
@@ -1,4 +1,19 @@
 // Copyright 2021 Rustamov Azer
 
+#include <random>
 #include "../../../modules/task_1/rustamov_a_histogram_stretch/histogram_stretch.h"
 
+
+
+Matrix generate_random_image(int w, int h, int bit = 8, int min_y = 50, int max_y = 150) {
+    std::random_device rd;
+    std::mt19937 mersenne(rd());
+    std::uniform_int_distribution<> rnd(min_y, max_y);
+    Matrix image(h * w);
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < w; j++) {
+            image[i * w + j] = static_cast<int>(rnd(mersenne));
+        }
+    }
+    return image;
+}
--- modules/task_1/rustamov_a_histogram_stretch/main.cpp
+++ modules/task_1/rustamov_a_histogram_stretch/main.cpp
@@ -1,3 +1,18 @@
 // Copyright 2021 Rustamov Azer
 
-#include <gtest/gtest.h>
+//#include <gtest/gtest.h>
+#include <iostream>
+#include "./histogram_stretch.h"
+
+int main() {
+    std::cout << "START" << std::endl;
+    int w = 10;
+    int h = 10;
+    Matrix image = generate_random_image(w, h);
+    for(int i = 1; i < w; i++) {
+        for(int j = 0; j < h; j++) {
+            std::cout << image[i * w + j] << "; ";
+        }
+        std::cout << std::endl;
+    }
+}
