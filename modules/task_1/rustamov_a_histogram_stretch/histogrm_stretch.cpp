b0ad19c1fa9a94ca476d6fd703d7a7d8e766354b
RustamovAzer <43135865+RustamovAzer@users.noreply.github.com>
2021-03-24 18:39:45 +0300

    throw runtime_error

--- modules/task_1/rustamov_a_histogram_stretch/histogrm_stretch.cpp
+++ modules/task_1/rustamov_a_histogram_stretch/histogrm_stretch.cpp
@@ -9,7 +9,7 @@
 Matrix generate_random_image(int w, int h, int min_y, int max_y) {
     if ((w <= 0) || (h <= 0) || (min_y < 0) || (max_y < 0) ||
         (min_y > 255) || (max_y > 255)) {
-        throw std::exception("Incorrect input for 'generate_random_image'");
+        throw std::runtime_error("Incorrect input for 'generate_random_image'");
     }
     std::random_device rd;
     std::mt19937 mersenne(rd());
@@ -26,7 +26,7 @@ Matrix generate_random_image(int w, int h, int min_y, int max_y) {
 
 Matrix make_histogram(const Matrix& image, int w, int h) {
     if ((w <= 0) || (h <= 0))
-        throw std::exception("Incorrect input for 'make_histogram'");
+        throw std::runtime_error("Incorrect input for 'make_histogram'");
     Matrix histogram(256);
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
@@ -60,7 +60,7 @@ int get_max_y(const Matrix& histogram) {
 
 Matrix stretch_histogram(const Matrix& histogtram, const int& min_y, const int& max_y) {
     if (min_y >= max_y)
-        throw std::exception("Cannot stretch histohram with provided min_y and max_y");
+        throw std::runtime_error("Cannot stretch histohram with provided min_y and max_y");
     Matrix result_histogram(256);
     for (int i = min_y; i < max_y + 1; i++) {
         result_histogram[255 * (i - min_y) / (max_y - min_y)] = histogtram[i];
@@ -70,9 +70,9 @@ Matrix stretch_histogram(const Matrix& histogtram, const int& min_y, const int& 
 
 Matrix increase_contrast(const Matrix& image, int w, int h, const int& min_y, const int& max_y) {
     if ((w <= 0) || (h <= 0))
-        throw std::exception("Incorrect input for 'increase_contrast'");
+        throw std::runtime_error("Incorrect input for 'increase_contrast'");
     if ((min_y >= max_y) || ((min_y == 0) && (max_y == 255)))
-        throw std::exception("Cannot stretch histohram with provided min_y and max_y");
+        throw std::runtime_error("Cannot stretch histohram with provided min_y and max_y");
     Matrix result_image(h * w);
     for (int i = 0; i < h; i++) {
         for (int j = 0; j < w; j++) {
@@ -84,7 +84,7 @@ Matrix increase_contrast(const Matrix& image, int w, int h, const int& min_y, co
 
 Matrix histogram_sretch_algorithm(const Matrix& image, const int w, const int h) {
     if ((w <= 0) || (h <= 0))
-        throw std::exception("Incorrect input for 'make_histogram'");
+        throw std::runtime_error("Incorrect input for 'make_histogram'");
     Matrix histogram = make_histogram(image, w, h);
     int min_y, max_y;
     min_y = get_min_y(histogram);
