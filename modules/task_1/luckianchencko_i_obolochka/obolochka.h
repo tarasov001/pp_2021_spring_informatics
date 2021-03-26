46ae4e881e5bca49d8d72bc8c138468403b79672
Ivan Luckianchencko <ivan.luckianchencko6879@yandex.ru>
2021-03-26 21:26:22 +0300

    first

---
+++ modules/task_1/luckianchencko_i_obolochka/obolochka.h
@@ -0,0 +1,16 @@
+// Copyright 2021 Luckianchencko Ivan
+#ifndef MODULES_TASK_1_LUCKIANCHENCKO_I_OBOLOCHKA_OBOLOCHKA_H_
+#define MODULES_TASK_1_LUCKIANCHENCKO_I_OBOLOCHKA_OBOLOCHKA_H_
+
+#include<vector>
+#include<cstdint>
+using mat = std::vector<std::vector<int>>;
+struct point {
+    int x, y;
+};
+double distance(point a, point b);
+double cosAngle(point a, point b, point c);
+std::vector<point> Jarvis_alg(mat a);
+std::vector<point> get_points(mat img);
+
+#endif  // MODULES_TASK_1_LUCKIANCHENCKO_I_OBOLOCHKA_OBOLOCHKA_H_
