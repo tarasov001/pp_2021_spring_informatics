43aa3d25957c58d8f4bf0c57a491dcc0e3fa29e9
sergey-makarichev <sergey_mak@mail.ru>
2021-03-28 00:03:26 +0300

    change3

--- modules/task_1/makarychev_s_bitwise_sort/bitwise_sort.h
+++ modules/task_1/makarychev_s_bitwise_sort/bitwise_sort.h
@@ -1,6 +1,6 @@
 // Copyright 2021 Makarychev Sergey
 #ifndef  MODULES_TASK_1_MAKARYCHEV_S_BITWISE_SORT_BITWISE_SORT_H_
-#define MODULES_TASK_1_MAKARYCHEV_S_FOXES_BITWISE_SORT_H_
+#define MODULES_TASK_1_MAKARYCHEV_S_BITWISE_SORT_BITWISE_SORT_H_
 #include <iostream>
 #include <vector>
 
@@ -9,4 +9,4 @@ void createCounters(int* sortVec, int* counters, int sizeVec);
 void signedRadix(int byteNumber, int sizeVec, int* sourceVec, int* destVec, int* count);
 void signedRadixSort(int* sortVec, int sizeVec);
 
-#endif  // ! MODULES_TASK_1_MAKARYCHEV_S_FOXES_BITWISE_SORT_H_
+#endif  // ! MODULES_TASK_1_MAKARYCHEV_S_BITWISE_SORT_BITWISE_SORT_H_
