a912ddec688803eaa51a005d391aa5a74650e539
Nikita Kuznetsov <nikita0bond@gmail.com>
2021-03-27 11:30:45 +0300

    add header file

--- modules/task_1/kuznetsov_n_mult_sparse_mat/sparse_matrix.h
+++ modules/task_1/kuznetsov_n_mult_sparse_mat/sparse_matrix.h
@@ -1,3 +1,18 @@
 // Copyright 2021 Kuznetsov Nikita
 #ifndef MODULES_TASK_1_KUZNETSOV_N_MULT_SPARSE_MAT_H_
 #define MODULES_TASK_1_KUZNETSOV_N_MULT_SPARSE_MAT_H_
+
+#include <vector>
+
+struct sparseMatrix {
+  std::vector<double> val;
+  std::vector<int> JA;
+  std::vector<int> IA;
+  int cols, rows, not_null;
+  friend const std::vector<double> operator*(const sparseMatrix& A, const sparseMatrix& B);
+};
+
+sparseMatrix CCS(const std::vector<double> new_mat, const int new_cols, const int new_rows);
+std::vector<double> randMat(const int rows, const int cols);
+
+#endif MODULES_TASK_1_KUZNETSOV_N_MULT_SPARSE_MAT_H_
