a52bf72715b846380e91b575086f61bd2df11191
dasha-ludina <zxsa098721@gmail.com>
2021-03-27 14:43:45 +0300

    Task_1

---
+++ modules/task_1/ludina_d_sparse_matrix_complex_ccs/sparse_matrix_complex_ccs.h
@@ -0,0 +1,27 @@
+// Copyright 2021 Ludina Daria
+#ifndef MODULES_TASK_1_LUDINA_D_SPARSE_MATRIX_COMPLEX_CCS_SPARSE_MATRIX_COMPLEX_CCS_H_
+#define MODULES_TASK_1_LUDINA_D_SPARSE_MATRIX_COMPLEX_CCS_SPARSE_MATRIX_COMPLEX_CCS_H_
+
+#include <vector>
+#include <complex>
+#include <random>
+#include <ctime>
+
+struct SparseMatrix {
+  int rows, cols;
+  std::vector<int> col_idx;
+  std::vector<int> row;
+  std::vector<std::complex<int>> value;
+
+  SparseMatrix();
+  SparseMatrix(int _rows, int _cols);
+  SparseMatrix(int _rows, int _cols,
+    std::vector<std::complex<int>> _value,
+    std::vector<int> _col_idx, std::vector<int> _row);
+  SparseMatrix(int _rows, int _cols, std::vector<std::complex<int>> matrix);
+  SparseMatrix transpose();
+};
+
+SparseMatrix Multiplication(SparseMatrix A, SparseMatrix B);
+
+#endif  // MODULES_TASK_1_LUDINA_D_SPARSE_MATRIX_COMPLEX_CCS_SPARSE_MATRIX_COMPLEX_CCS_H_
