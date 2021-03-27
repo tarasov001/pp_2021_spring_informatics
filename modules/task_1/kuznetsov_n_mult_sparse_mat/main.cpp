0eba66f72e7c1545815498316a89d32e498ae875
Nikita Kuznetsov <nikita0bond@gmail.com>
2021-03-27 12:17:01 +0300

    done lab 1

--- modules/task_1/kuznetsov_n_mult_sparse_mat/main.cpp
+++ modules/task_1/kuznetsov_n_mult_sparse_mat/main.cpp
@@ -2,4 +2,36 @@
 #include <gtest/gtest.h>
 #include <vector>
 #include "./sparse_matrix.h"
+#define SIZE 100
 
+TEST(MULTIPLY_SPARSE_MATRIX, THROWS_WHEN_SIZE_IS_NEGATIVE) {
+  ASSERT_ANY_THROW(randMat(-10, -10));
+}
+
+TEST(MULTIPLY_SPARSE_MATRIX, THROWS_WHEN_SIZE_IS_ZERO) {
+  ASSERT_ANY_THROW(randMat(0, 0));
+}
+
+TEST(MULTIPLY_SPARSE_MATRIX, SOLUTION_EQUAL_EXPECTED_RESULT) {
+  sparseMatrix A = CCS(std::vector<double>{0, 0, 1, 0, 2, 0, 0, 0, 3, 0, 0, 0}, 3, 4);
+  sparseMatrix B = CCS(std::vector<double>{3, 0, 0, 0, 4, 0}, 2, 3);
+  std::vector<double> exp_res = { 4, 0, 0, 0, 12, 0, 0, 0 };
+  std::vector<double> res = A * B;
+  ASSERT_EQ(res, exp_res);
+}
+
+TEST(MULTIPLY_SPARSE_MATRIX, SOLUTION_WITH_50_ELEM_NO_THROW) {
+  sparseMatrix A, B;
+  A = CCS(randMat(50, 50), 50, 50);
+  B = CCS(randMat(50, 50), 50, 50);
+  std::vector<double> res = A * B;
+  ASSERT_NO_THROW(res);
+}
+
+TEST(MULTIPLY_SPARSE_MATRIX, SOLUTION_WITH_N_ELEM_NO_THROW) {
+  sparseMatrix A, B;
+  A = CCS(randMat(SIZE, SIZE), SIZE, SIZE);
+  B = CCS(randMat(SIZE, SIZE), SIZE, SIZE);
+  std::vector<double> res = A * B;
+  ASSERT_NO_THROW(res);
+}
--- modules/task_1/kuznetsov_n_mult_sparse_mat/sparse_matrix.cpp
+++ modules/task_1/kuznetsov_n_mult_sparse_mat/sparse_matrix.cpp
@@ -1,2 +1,69 @@
 // Copyright 2021 Kuznetsov Nikita
+#include <vector>
+#include <random>
+#include <ctime>
+#include "../../../modules/task_1/kuznetsov_n_mult_sparse_mat/sparse_matrix.h"
 
+std::vector<double> randMat(const int cols, const int rows) {
+  if (rows <= 0 || cols <= 0) {
+    throw - 1;
+  }
+  std::srand(std::time(nullptr));
+  std::vector<double> res(cols * rows);
+  for (int i = 0; i < rows * cols; i++) {
+    double val_rand = static_cast<double>(std::rand() % 50 + 1);
+    if (val_rand < 4) {
+      res[i] = val_rand;
+    } else {
+      res[i] = 0;
+    }
+  }
+  return res;
+}
+
+sparseMatrix CCS(const std::vector<double> new_mat, const int new_cols, const int new_rows) {
+  if (new_cols <= 0 || new_rows <= 0) {
+    throw - 1;
+  }
+  sparseMatrix res;
+  res.cols = new_cols;
+  res.rows = new_rows;
+  res.not_null = 0;
+  res.JA.push_back(0);
+  for (int column = 0; column < new_cols; column++) {
+    int not_null_count = 0;
+    for (int i = column; i <= (new_rows - 1) * new_cols + column;
+      i += new_cols) {
+      if (new_mat[i] != 0) {
+        not_null_count++;
+        res.val.push_back(new_mat[i]);
+        res.IA.push_back((i - column) / new_cols);
+      }
+    }
+    res.JA.push_back(res.JA.back() + not_null_count);
+    res.not_null += not_null_count;
+  }
+  return res;
+}
+
+const std::vector<double> operator*(const sparseMatrix& A, const sparseMatrix& B) {
+  if (A.cols != B.rows) {
+    throw - 1;
+  }
+  std::vector<double> result(A.rows * B.cols, 0);
+  for (int col = 0; col < A.cols; col++) {
+    for (int b_col = 0; b_col < B.cols; b_col++) {
+      for (int i = A.JA[col]; i <= A.JA[col + 1] - 1; i++) {
+        if (B.JA[b_col + 1] - B.JA[b_col] == 0) {
+          continue;
+        }
+        for (int j = B.JA[b_col]; j <= B.JA[b_col + 1] - 1; j++) {
+          if (B.IA[j] == col) {
+            result[A.IA[i] * B.cols + b_col] += A.val[i] * B.val[j];
+          }
+        }
+      }
+    }
+  }
+  return result;
+}
