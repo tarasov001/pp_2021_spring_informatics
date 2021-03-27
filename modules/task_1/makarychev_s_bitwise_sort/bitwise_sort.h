6361be03c7355499ac76414cc7821aae4ddbad14
sergey-makarichev <sergey_mak@mail.ru>
2021-03-28 00:11:03 +0300

    change4

--- modules/task_1/makarychev_s_bitwise_sort/bitwise_sort.h
+++ modules/task_1/makarychev_s_bitwise_sort/bitwise_sort.h
@@ -9,4 +9,4 @@ void createCounters(int* sortVec, int* counters, int sizeVec);
 void signedRadix(int byteNumber, int sizeVec, int* sourceVec, int* destVec, int* count);
 void signedRadixSort(int* sortVec, int sizeVec);
 
-#endif  // ! MODULES_TASK_1_MAKARYCHEV_S_BITWISE_SORT_BITWISE_SORT_H_
+#endif  // MODULES_TASK_1_MAKARYCHEV_S_BITWISE_SORT_BITWISE_SORT_H_
