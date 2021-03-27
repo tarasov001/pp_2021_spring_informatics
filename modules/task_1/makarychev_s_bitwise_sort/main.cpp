fd909ea2bd99a83fdded84d4fb5518e916887e46
sergey-makarichev <sergey_mak@mail.ru>
2021-03-27 23:54:39 +0300

    change2

--- modules/task_1/makarychev_s_bitwise_sort/main.cpp
+++ modules/task_1/makarychev_s_bitwise_sort/main.cpp
@@ -21,7 +21,7 @@ TEST(Sequential_Radix_Sort, sort_vector_of_random_value_2) {
 }
 
 TEST(Sequential_Radix_Sort, sort_vector_of_litle_positive_value) {
-    std::vector<int> vec1 = {1,3,7,4,0,7,0,9};
+    std::vector<int> vec1 = {1, 3, 7, 4, 0, 7, 0, 9};
     std::vector<int> vec2 = vec1;
     std::sort(vec2.begin(), vec2.begin() + vec2.size());
     signedRadixSort(vec1.data(), vec1.size());
@@ -29,7 +29,7 @@ TEST(Sequential_Radix_Sort, sort_vector_of_litle_positive_value) {
 }
 
 TEST(Sequential_Radix_Sort, sort_vector_of_litle_negative_value) {
-    std::vector<int> vec1 = { -1,-8,-4,-10,-1,-6,-6,-6 };
+    std::vector<int> vec1 = { -1, -8, -4, -10, -1, -6, -6, -6 };
     std::vector<int> vec2 = vec1;
     signedRadixSort(vec1.data(), vec1.size());
     std::sort(vec2.begin(), vec2.begin() + vec2.size());
@@ -37,7 +37,7 @@ TEST(Sequential_Radix_Sort, sort_vector_of_litle_negative_value) {
 }
 
 TEST(Sequential_Radix_Sort, sort_vector_of_zero_value) {
-    std::vector<int> vec1 = { 0,0,0,0,0,0,0,0};
+    std::vector<int> vec1 = { 0, 0, 0, 0, 0, 0, 0, 0};
     std::vector<int> vec2 = vec1;
     signedRadixSort(vec1.data(), vec1.size());
     std::sort(vec2.begin(), vec2.begin() + vec2.size());
@@ -45,7 +45,7 @@ TEST(Sequential_Radix_Sort, sort_vector_of_zero_value) {
 }
 
 TEST(Sequential_Radix_Sort, sort_vector_of_mixed_value) {
-    std::vector<int> vec1 = { 2,-99947,-9,87456,-987456,0 };
+    std::vector<int> vec1 = { 2, -99947, -9, 87456, -987456, 0 };
     std::vector<int> vec2 = vec1;
     signedRadixSort(vec1.data(), vec1.size());
     std::sort(vec2.begin(), vec2.begin() + vec2.size());
@@ -53,7 +53,7 @@ TEST(Sequential_Radix_Sort, sort_vector_of_mixed_value) {
 }
 
 TEST(Sequential_Radix_Sort, sort_just_sorted_vector) {
-    std::vector<int> vec1 = { -99999,-1,0,8,9,15,10265,235458 };
+    std::vector<int> vec1 = { -99999, -1, 0, 8, 9, 15, 10265, 235458 };
     std::vector<int> vec2 = vec1;
     signedRadixSort(vec1.data(), vec1.size());
     std::sort(vec2.begin(), vec2.begin() + vec2.size());
