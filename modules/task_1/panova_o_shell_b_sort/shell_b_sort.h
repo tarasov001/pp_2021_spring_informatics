3cc65202ff16298f1c71d3c9b0c1be7d18dc48b5
OlgaMakesThings <56171499+OlgaMakesThings@users.noreply.github.com>
2021-03-27 16:24:35 +0300

    last i swear

--- modules/task_1/panova_o_shell_b_sort/shell_b_sort.h
+++ modules/task_1/panova_o_shell_b_sort/shell_b_sort.h
@@ -8,6 +8,6 @@ std::vector<int> CreateVector(int size);
 std::vector<int> EvenMerge(const std::vector<int>& left, const std::vector<int>& right);
 std::vector<int> OddMerge(const std::vector<int>& left, const std::vector<int>& right);
 std::vector<int> BatcherMerge(const std::vector<int>& left, const std::vector<int>& right);
-std::vector<int> ShellSort(std::vector<int>& a);
+std::vector<int> ShellSort(const std::vector<int>& a);
 std::vector<int> OddEvenShellSort(const std::vector<int>& a);
 #endif  // MODULES_TASK_1_PANOVA_O_SHELL_B_SORT_SHELL_B_SORT_H_
