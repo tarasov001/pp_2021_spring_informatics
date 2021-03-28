5e1d25e8ecf5f1ee60918570035f493d0b42f3fd
zaitcev-andrey <andrey_zaycev_2000@mail.ru>
2021-03-28 10:57:13 +0300

    fix style

--- modules/task_1/zaitsev_a_radix_sort_seq/main.cpp
+++ modules/task_1/zaitsev_a_radix_sort_seq/main.cpp
@@ -5,105 +5,106 @@
 #include "./RadixSortSeq.h"
 
 TEST(Test_Seq, Vector_One_El) {
-	int size = 1;
-	std::vector<double> vec = createVector(size, 0.0, 10.0);
-	double* tmp = new double[size];
-	std::vector<double> vec2(vec);
-	radixSort(vec.data(), tmp, size);
-	delete[] tmp;
-	std::sort(vec2.begin(), vec2.end());
-	ASSERT_EQ(vec2, vec);
+  int size = 1;
+  std::vector<double> vec = createVector(size, 0.0, 10.0);
+  double* tmp = new double[size];
+  std::vector<double> vec2(vec);
+  radixSort(vec.data(), tmp, size);
+  delete[] tmp;
+  std::sort(vec2.begin(), vec2.end());
+  ASSERT_EQ(vec2, vec);
 }
 
 TEST(Test_Seq, Size_5_Without_Random) {
-	int size = 5;
-	std::vector<double> vec = { 4.33, -6.541, 16.84, 2.662, -12.544 };
-	double* tmp = new double[size];
-	std::vector<double> vec2(vec);
-	radixSort(vec.data(), tmp, size);
-	delete[] tmp;
-	std::sort(vec2.begin(), vec2.end());
-	ASSERT_EQ(vec2, vec);
+  int size = 5;
+  std::vector<double> vec = { 4.33, -6.541, 16.84, 2.662, -12.544 };
+  double* tmp = new double[size];
+  std::vector<double> vec2(vec);
+  radixSort(vec.data(), tmp, size);
+  delete[] tmp;
+  std::sort(vec2.begin(), vec2.end());
+  ASSERT_EQ(vec2, vec);
 }
 
 TEST(Test_Seq, Size_1000_Random_Positive) {
-	int size = 1000;
-	std::vector<double> vec = createVector(size, 100.0, 1000.0);
-	double* tmp = new double[size];
-	std::vector<double> vec2(vec);
-	radixSort(vec.data(), tmp, size);
-	delete[] tmp;
-	std::sort(vec2.begin(), vec2.end());
-	ASSERT_EQ(vec2, vec);
+  int size = 1000;
+  std::vector<double> vec = createVector(size, 100.0, 1000.0);
+  double* tmp = new double[size];
+  std::vector<double> vec2(vec);
+  radixSort(vec.data(), tmp, size);
+  delete[] tmp;
+  std::sort(vec2.begin(), vec2.end());
+  ASSERT_EQ(vec2, vec);
 }
 
 TEST(Test_Seq, Size_1000_Random_Negative) {
-	int size = 1000;
-	std::vector<double> vec = createVector(size, -800.0, 0.0);
-	double* tmp = new double[size];
-	std::vector<double> vec2(vec);
-	radixSort(vec.data(), tmp, size);
-	delete[] tmp;
-	std::sort(vec2.begin(), vec2.end());
-	ASSERT_EQ(vec2, vec);
+  int size = 1000;
+  std::vector<double> vec = createVector(size, -800.0, 0.0);
+  double* tmp = new double[size];
+  std::vector<double> vec2(vec);
+  radixSort(vec.data(), tmp, size);
+  delete[] tmp;
+  std::sort(vec2.begin(), vec2.end());
+  ASSERT_EQ(vec2, vec);
 }
 
 TEST(Test_Seq, Size_1000_Random_Pos_And_Neg) {
-	int size = 1000;
-	std::vector<double> vec = createVector(size, -500.0, 500.0);
-	double* tmp = new double[size];
-	std::vector<double> vec2(vec);
-	radixSort(vec.data(), tmp, size);
-	delete[] tmp;
-	std::sort(vec2.begin(), vec2.end());
-	ASSERT_EQ(vec2, vec);
+  int size = 1000;
+  std::vector<double> vec = createVector(size, -500.0, 500.0);
+  double* tmp = new double[size];
+  std::vector<double> vec2(vec);
+  radixSort(vec.data(), tmp, size);
+  delete[] tmp;
+  std::sort(vec2.begin(), vec2.end());
+  ASSERT_EQ(vec2, vec);
 }
 
 TEST(Test_Seq, Exact_Answer) {
-	int size = 7;
-	std::vector<double> vec = {5.32, 8.25, 3.463, 43.733, 120.632, 75.86, 53.41};
-	std::vector<double> answer = {3.463, 5.32, 8.25, 43.733, 53.41, 75.86, 120.632};
-	double* tmp = new double[size];
-	radixSort(vec.data(), tmp, size);
-	delete[] tmp;
-	for(int i = 0; i < size; i++)
-		ASSERT_NEAR(vec[i], answer[i], 0.01);
+  int size = 7;
+  std::vector<double> vec = {5.32, 8.25, 3.463, 43.733, 120.632, 75.86, 53.41};
+  std::vector<double> answer =
+  {3.463, 5.32, 8.25, 43.733, 53.41, 75.86, 120.632};
+  double* tmp = new double[size];
+  radixSort(vec.data(), tmp, size);
+  delete[] tmp;
+  for (int i = 0; i < size; i++)
+    ASSERT_NEAR(vec[i], answer[i], 0.01);
 }
 
 TEST(Test_Seq, Size_100_Batcher) {
-	int size = 100;
-	double* tmp = new double[size];
-	std::vector<double> vec = createVector(size, 0.0, 1000.0);
-	std::vector<double> vec2(vec);
-	oddEvenMergeSort(vec.data(), tmp, size, 6);
-	delete[] tmp;
-	std::sort(vec2.begin(), vec2.end());
-	ASSERT_EQ(vec2, vec);
+  int size = 100;
+  double* tmp = new double[size];
+  std::vector<double> vec = createVector(size, 0.0, 1000.0);
+  std::vector<double> vec2(vec);
+  oddEvenMergeSort(vec.data(), tmp, size, 6);
+  delete[] tmp;
+  std::sort(vec2.begin(), vec2.end());
+  ASSERT_EQ(vec2, vec);
 }
 
 TEST(Test_Seq, Size_1000_Batcher_Border_20) {
-	int size = 1000;
-	double* tmp = new double[size];
-	std::vector<double> vec = createVector(size, -1000.0, 1000.0);
-	std::vector<double> vec2(vec);
-	oddEvenMergeSort(vec.data(), tmp, size, 20);
-	delete[] tmp;
-	std::sort(vec2.begin(), vec2.end());
-	ASSERT_EQ(vec2, vec);
+  int size = 1000;
+  double* tmp = new double[size];
+  std::vector<double> vec = createVector(size, -1000.0, 1000.0);
+  std::vector<double> vec2(vec);
+  oddEvenMergeSort(vec.data(), tmp, size, 20);
+  delete[] tmp;
+  std::sort(vec2.begin(), vec2.end());
+  ASSERT_EQ(vec2, vec);
 }
 
 TEST(Test_Seq, Size_1000_Batcher_Border_1000) {
-	int size = 1000;
-	double* tmp = new double[size];
-	std::vector<double> vec = createVector(size, -1000.0, 1000.0);
-	std::vector<double> vec2(vec);
-	oddEvenMergeSort(vec.data(), tmp, size, 1000);
-	delete[] tmp;
-	std::sort(vec2.begin(), vec2.end());
-	ASSERT_EQ(vec2, vec);
+  int size = 1000;
+  double* tmp = new double[size];
+  std::vector<double> vec = createVector(size, -1000.0, 1000.0);
+  std::vector<double> vec2(vec);
+  oddEvenMergeSort(vec.data(), tmp, size, 1000);
+  delete[] tmp;
+  std::sort(vec2.begin(), vec2.end());
+  ASSERT_EQ(vec2, vec);
 }
 
 int main(int argc, char** argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
