de790cca471af6d36ca8c136a020ec1525abcb26
Yana Streltsova <43129466+streltsova-yana@users.noreply.github.com>
2021-03-29 00:01:28 +0300

    Update Shell_sort.cpp

--- modules/task_1/streltsova_y_Shell_sort/Shell_sort.cpp
+++ modules/task_1/streltsova_y_Shell_sort/Shell_sort.cpp
@@ -9,6 +9,7 @@ std::vector<int> getRandomVector(int size) {
     std::mt19937 gen(time(0));
     gen.seed(static_cast<unsigned int>(time(0)));
     std::vector<int> vec(size);
+
     for (int i = 0; i < size; i++) { vec[i] = gen() % 100; }
     return vec;
 }
