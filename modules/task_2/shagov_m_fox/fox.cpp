a8857f885c9c307ba680716dc8d0eef0c76c482c
MaksimShagov <makcim.shagov@mail.ru>
2021-03-28 10:48:17 +0400

    without tests

---
+++ modules/task_2/shagov_m_fox/fox.cpp
@@ -0,0 +1,116 @@
+// Copyright 2021 Shagov Maksim
+#include "../../../modules/task_2/shagov_m_fox/fox.h"
+#include <omp.h>
+
+bool isEqual(double x, double y) {
+    return std::fabs(x - y) < 0.001;
+}
+
+bool isEqualMatrix(Matrix A, Matrix B) {
+    if ((A.size() <= 0) || (B.size() <= 0))
+        throw "Size of matrix must be > 0";
+    if (A.size() != B.size())
+        throw "Different size of matrix";
+    size_t size = A.size();
+    for (size_t i = 0; i < size; i++)
+        if (!isEqual(A[i], B[i]))
+            return false;
+    return true;
+}
+
+Matrix createRandomMatrix(size_t size) {
+    if (size <= 0)
+        throw "Size of matrix must be > 0";
+    Matrix result(size, 0);
+    std::random_device rd;
+    std::mt19937 mersenne(rd());
+    std::uniform_real_distribution<> urd(-100.0, 100.0);
+    for (size_t i = 0; i < size; i++) {
+        result[i] = static_cast<double>(urd(mersenne));
+    }
+    return result;
+}
+
+Matrix sequentialMatrixMultiplication(std::vector<double> A, std::vector<double> B, size_t Size) {
+    if (Size <= 0)
+        throw "Block size of matrix must be > 0";
+    if ((A.size() <= 0) || (B.size() <= 0))
+        throw "Size of matrix must be > 0";
+    if (A.size() != B.size())
+        throw "Different size of matrix";
+    if ((A.size() != Size) || (B.size() != Size))
+        throw "Different param and size";
+    size_t BlockSize = static_cast<size_t>(sqrt(Size));
+    Matrix result(BlockSize * BlockSize, 0);
+    for (size_t i = 0; i < BlockSize; i++)
+        for (size_t j = 0; j < BlockSize; j++)
+            for (size_t k = 0; k < BlockSize; k++)
+                result[i * BlockSize  + j] += A[i * BlockSize + k] * B[k * BlockSize + j];
+    return result;
+}
+
+Matrix sequentialBlockMatrixMultiplication(std::vector<double> A, std::vector<double> B, size_t Size) {
+    if (Size <= 0)
+        throw "Block size of matrix must be > 0";
+    if ((A.size() <= 0) || (B.size() <= 0))
+        throw "Size of matrix must be > 0";
+    if (A.size() != B.size())
+        throw "Different size of matrix";
+    if ((A.size() != Size) || (B.size() != Size))
+        throw "Different param and size";
+    if (static_cast<size_t>(sqrt(Size)) * static_cast<size_t>(sqrt(Size)) != Size)
+        throw "Size not square";
+    size_t BlockSize = static_cast<size_t>(sqrt(Size));
+    size_t BlockCount = static_cast<size_t>(BlockSize/static_cast<size_t>(sqrt(4))) == 0
+                       ? 1 : static_cast<size_t>(BlockSize/static_cast<size_t>(sqrt(4)));
+    Matrix result(BlockSize * BlockSize, 0);
+    for (size_t i = 0; i < BlockSize; i += BlockCount)
+        for (size_t j = 0; j < BlockSize; j += BlockCount)
+            for (size_t k = 0; k < BlockSize; k += BlockCount)
+                for (size_t ii = i; ii < ((BlockCount + i) % BlockSize + BlockCount); ii++)
+                    for (size_t jj = j; jj < ((BlockCount + j) % BlockSize + BlockCount); jj++)
+                        for (size_t kk = k; kk < ((BlockCount + k) % BlockSize + BlockCount); kk++)
+                            result[ii * BlockSize  + jj] += A[ii * BlockSize + kk] * B[kk * BlockSize + jj];
+    return result;
+}
+
+Matrix parallelBlockMatrixMultiplication(std::vector<double> A, std::vector<double> B, int Size) {
+    if (Size <= 0)
+        throw "Block size of matrix must be > 0";
+    if ((A.size() <= 0) || (B.size() <= 0))
+        throw "Size of matrix must be > 0";
+    if (A.size() != B.size())
+        throw "Different size of matrix";
+    if ((A.size() != Size) || (B.size() != Size))
+        throw "Different param and size";
+    if (static_cast<int>(sqrt(Size)) * static_cast<int>(sqrt(Size)) != Size)
+        throw "Size not square";
+    int stage;
+    Matrix result(Size, 0);
+    int cols = static_cast<int>(sqrt(Size));
+    #pragma omp parallel private(stage) shared(A, B, result)
+    {
+        
+        int threads_count = omp_get_num_threads();
+        int blocks_count = sqrt(threads_count);
+        int block_cols_size = cols / blocks_count;
+        int thread_num = omp_get_thread_num();
+        int i1 = thread_num / blocks_count, j1 = thread_num % blocks_count;
+        double *A1, *B1, *C1;
+        for (stage = 0; stage < blocks_count; stage++) {
+            A1 = A.data() + (i1 * cols + ((i1 + stage) % blocks_count)) * block_cols_size;
+            B1 = B.data() + (((i1 + stage) % blocks_count) * cols + j1) * block_cols_size;
+            C1 = result.data() + (i1 * cols + j1) * block_cols_size;
+            for (int i = 0; i < block_cols_size; i++) {
+                for (int j = 0; j < block_cols_size; j++) {
+                    double tmp = 0.0;
+                    for (int k = 0; k < block_cols_size; k++) {
+                        tmp += *(A1 + i * cols + k) * *(B1 + k * cols + j);
+                    }
+                    *(C1 + i * cols + j) += tmp;
+                }   
+            }
+        }
+    }
+    return result;
+}
---
+++ modules/task_2/shagov_m_fox/fox.h
@@ -0,0 +1,21 @@
+// Copyright 2021 Shagov Maksim
+#ifndef MODULES_TASK_2_SHAGOV_M_FOX_FOX_H_
+#define MODULES_TASK_2_SHAGOV_M_FOX_FOX_H_
+#include <omp.h>
+#include <vector>
+#include <iostream>
+#include <ctime>
+#include <random>
+#include <cmath>
+#include <limits>
+
+typedef  std::vector<double> Matrix;
+
+bool isEqual(double x, double y);
+bool isEqualMatrix(Matrix A, Matrix B);
+
+Matrix createRandomMatrix(size_t size);
+Matrix sequentialMatrixMultiplication(std::vector<double> A, std::vector<double> B, size_t BlockSize);
+Matrix sequentialBlockMatrixMultiplication(std::vector<double> A, std::vector<double> B, size_t Size);
+Matrix parallelBlockMatrixMultiplication(std::vector<double> A, std::vector<double> B, int Size);
+#endif  // MODULES_TASK_2_SHAGOV_M_FOX_FOX_H_
