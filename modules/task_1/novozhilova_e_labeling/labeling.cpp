405047a6f6554c72ed2dc97d5e9c97b677cd6d0b
delarinq <eks-pliz@ya.ru>
2021-03-29 01:10:54 +0300

    second

--- modules/task_1/novozhilova_e_labeling/labeling.cpp
+++ modules/task_1/novozhilova_e_labeling/labeling.cpp
@@ -49,7 +49,6 @@ std::vector<std::vector<int>> Labeling(std::vector<std::vector<int>> arr, int ro
                          lab[i - 1][j] = minim;
                          label = minim;
                     }
-
                }
           }
      }
