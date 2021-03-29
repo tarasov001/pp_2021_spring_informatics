fc9f35f62ef87802abb65ffad7fa04ebbd320ae1
Sergey01923 <s.bessolitsyn@mail.ru>
2021-03-29 15:21:24 +0300

    qq

---
+++ modules/task_1/bessolitsyn_s_quick_sort/quick_sort.cpp
@@ -0,0 +1,26 @@
+// Copyright 2021 Bessolytsin Sergay
+
+#include <random>
+
+#include "../../../modules/task_1/bessolitsyn_s_quick_sort/quick_sort.h"
+
+std::vector<int> getRandomVector(int size, uint64_t seed) {
+    std::vector<int> vec(size);
+    std::mt19937 gen;
+    gen.seed(seed);
+    for (int i = 0; i < size; ++i) {
+        vec[i] = gen() % 10000;
+    }
+    return vec;
+}
+
+std::vector<double> getRandomDoubleVector(int size, uint64_t seed) {
+    std::vector<double> vec(size);
+    std::mt19937 gen;
+    gen.seed(seed);
+    std::uniform_real_distribution<double> dist(-42.0, 42.0);
+    for (int i = 0; i < size; ++i) {
+        vec[i] = dist(gen);
+    }
+    return vec;
+}
