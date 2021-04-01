59db798ba21ee19b479b6358ff3d4d5a3afc4be2
Leroyku <mark.kumbrasev@gmail.com>
2021-04-01 21:24:39 +0300

    2

--- modules/task_1/kumbrasev_m_conjugate_gradient/conjugate_gradient.h
+++ modules/task_1/kumbrasev_m_conjugate_gradient/conjugate_gradient.h
@@ -7,10 +7,10 @@
 #include <vector>
 #include <numeric>
 
-vector<double> linVec( double a, const vector<double> &v1, double b, const vector<double> &v2 );
-double innerVec( const vector<double> &v1, const vector<double> &v2 );
-vector<double> mMultv( const vector<vector<double>> &M, const vector<double> &V );
-double vecNorm( const vector<double> &V );
-vector<double> ConjugateGradient( const vector<vector<double>> &M, const vector<double> &B );
+vector<double> linVec(double a, const vector<double> &v1, double b, const vector<double> &v2);
+double innerVec(const vector<double> &v1, const vector<double> &v2);
+vector<double> mMultv(const vector<vector<double>> &M, const vector<double> &V);
+double vecNorm(const vector<double> &V);
+vector<double> ConjugateGradient(const vector<vector<double>> &M, const vector<double> &B);
 
 #endif  // MODULES_TASK_1_KUMBRASEV_M_CONJUGATE_GRADIENT_CONJUGATE_GRADIENT_H_
--- modules/task_1/kumbrasev_m_conjugate_gradient/main.cpp
+++ modules/task_1/kumbrasev_m_conjugate_gradient/main.cpp
@@ -6,10 +6,9 @@ TEST(Conjugate_gradient, M2) {
     vector<vector<double>> M = { { 5, 2}, {2, 1} };
     vector<double> B = { 7, 9 };
 
-    vector<double> X = ConjugateGradient( M, B );
-    vector<double> res = mMultv( M, X );
-    
-    for (size_t i = 0; i < res.size(); ++i) {
+    vector<double> X = ConjugateGradient(M, B);
+    vector<double> res = mMultv(M, X);
+	for (size_t i = 0; i < res.size(); ++i) {
         ASSERT_EQ(res[i], B[i]);
     }
 }
@@ -17,10 +16,9 @@ TEST(Conjugate_gradient, X) {
     vector<vector<double>> M = { { 2, 3, -1 }, {1, -2, 1 }, { 1, 0, 2 } };
     vector<double> B = { 9, 3, 2 };
 
-    vector<double> X = ConjugateGradient( M, B );
+    vector<double> X = ConjugateGradient(M, B);
     vector<double> res = { 4, 0, -1 };
-    
-    for (size_t i = 0; i < res.size(); ++i) {
+	for (size_t i = 0; i < res.size(); ++i) {
         ASSERT_EQ(res[i], X[i]);
     }
 }
@@ -28,10 +26,9 @@ TEST(Conjugate_gradient, M3) {
     vector<vector<double>> M = { { 2, 3, -1 }, {1, -2, 1 }, { 1, 0, 2 } };
     vector<double> B = { 9, 3, 2 };
 
-    vector<double> X = ConjugateGradient( M, B );
-    vector<double> res = mMultv( M, X );
-    
-    for (size_t i = 0; i < res.size(); ++i) {
+    vector<double> X = ConjugateGradient(M, B);
+    vector<double> res = mMultv(M, X);
+	for (size_t i = 0; i < res.size(); ++i) {
         ASSERT_NEAR(B[i], res[i], 0.05);
     }
 }
@@ -39,10 +36,9 @@ TEST(Conjugate_gradient, M4) {
     vector<vector<double>> M = { { 1, -1, 3, 1 }, {4, -1, 5, 4 }, { 2, -2, 4, 1 }, { 1, -4, 5, -1 } };
     vector<double> B = { 5, 4, 6, 3 };
 
-    vector<double> X = ConjugateGradient( M, B );
-    vector<double> res = mMultv( M, X );
-    
-    for (size_t i = 0; i < res.size(); ++i) {
+    vector<double> X = ConjugateGradient(M, B);
+    vector<double> res = mMultv(M, X);
+	for (size_t i = 0; i < res.size(); ++i) {
         ASSERT_NE(B[i], res[i]);
     }
 }
@@ -50,10 +46,9 @@ TEST(Conjugate_gradient, MultiMB) {
     vector<vector<double>> M = { { 2, 6, 1 }, { 3, 1, 7 }, { 8, 9, 1 } };
     vector<double> B = { 7, 4, 1 };
 
-    vector<double> res = mMultv( M, B );
+    vector<double> res = mMultv(M, B);
     vector<double> I = { { 39, 32, 93 } };
-    
-    for (size_t i = 0; i < res.size(); ++i) {
+	for (size_t i = 0; i < res.size(); ++i) {
         ASSERT_EQ(I[i], res[i]);
     }
 }
