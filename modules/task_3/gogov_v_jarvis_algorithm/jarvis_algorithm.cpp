41ce03ca54403e235d83e0a50654f377388ed9b3
vlad-gogov <43129455+vlad-gogov@users.noreply.github.com>
2021-04-09 16:13:22 +0300

    using lambda expression

--- modules/task_3/gogov_v_jarvis_algorithm/jarvis_algorithm.cpp
+++ modules/task_3/gogov_v_jarvis_algorithm/jarvis_algorithm.cpp
@@ -1,10 +1,10 @@
 // Copyright 2021 Gogov Vladislav
-#include <random>
 #include "../../../modules/task_3/gogov_v_jarvis_algorithm/jarivs_algorithm.h"
+#include <random>
+
 
 int rotate(const Point& A, const Point& B, const Point& C) {
-    int64_t temp = (C.first - A.first) * (B.second - A.second) -
-                   (C.second - A.second) * (B.first - A.first);
+    int64_t temp = (C.first - A.first) * (B.second - A.second) - (C.second - A.second) * (B.first - A.first);
     if (temp > 0)
         return 1;
     else if (temp < 0)
@@ -35,14 +35,11 @@ Point searchBottomLeft(const std::vector<Point>& points) {
     return point_bottom_left;
 }
 
-bool checkPointsDistances(const Point& current, const Point& next,
-                          const Point& temp) {
-    size_t dist_nefirstt_current =
-        (next.first - current.first) * (next.first - current.first) +
-        (next.second - current.second) * (next.second - current.second);
-    size_t dist_temp_current =
-        (temp.first - current.first) * (temp.first - current.first) +
-        (temp.second - current.second) * (temp.second - current.second);
+bool checkPointsDistances(const Point& current, const Point& next, const Point& temp) {
+    size_t dist_nefirstt_current = (next.first - current.first) * (next.first - current.first) +
+                                   (next.second - current.second) * (next.second - current.second);
+    size_t dist_temp_current = (temp.first - current.first) * (temp.first - current.first) +
+                               (temp.second - current.second) * (temp.second - current.second);
     if (dist_nefirstt_current < dist_temp_current)
         return true;
     return false;
@@ -79,77 +76,6 @@ std::vector<Point> jarvisAlgorithmSeq(const std::vector<Point>& points) {
     return convex_hull;
 }
 
-class FirstPoint {
-    const std::vector<Point>& points;
-    Point base;
-
- public:
-    FirstPoint(const std::vector<Point>& points_, Point base_) : points(points_), base(base_) {
-    }
-
-    FirstPoint(const FirstPoint& r, tbb::split) : points(r.points), base(r.base) {
-    }
-
-    void operator()(const tbb::blocked_range<size_t>& r) {
-        auto begin = r.begin(), end = r.end();
-        for (auto i = begin; i != end; i++) {
-            if (points[i] < base)
-                base = points[i];
-        }
-    }
-
-    void join(const FirstPoint& r) {
-        if (r.base < base)
-            base = r.base;
-    }
-
-    Point getBase() {
-        return base;
-    }
-};
-
-class NextPoint {
-    Point next;
-    const std::vector<Point>& points;
-    Point last_point_hull;
-
- public:
-    NextPoint(const std::vector<Point>& points_, Point next_, Point last_point_hull_)
-        : next(next_), points(points_), last_point_hull(last_point_hull_) {
-    }
-
-    NextPoint(const NextPoint& r, tbb::split)
-        : next(r.next), points(r.points), last_point_hull(r.last_point_hull) {
-    }
-
-    void operator()(const tbb::blocked_range<size_t>& r) {
-        auto begin = r.begin(), end = r.end();
-        for (auto i = begin; i != end; i++) {
-            int temp = rotate(last_point_hull, next, points[i]);
-            if (temp > 0) {
-                next = points[i];
-            } else if (temp == 0) {
-                if (checkPointsDistances(last_point_hull, next, points[i]))
-                    next = points[i];
-            }
-        }
-    }
-
-    void join(const NextPoint& r) {
-        int temp = rotate(r.last_point_hull, next, r.next);
-        if (temp > 0) {
-            next = r.next;
-        } else if (temp == 0) {
-            if (checkPointsDistances(r.last_point_hull, next, r.next))
-                next = r.next;
-        }
-    }
-
-    Point getNext() {
-        return next;
-    }
-};
-
 std::vector<Point> jarvisAlgorithmTbb(const std::vector<Point>& points) {
     int count_points = static_cast<int>(points.size());
     if (count_points == 0)
@@ -157,9 +83,20 @@ std::vector<Point> jarvisAlgorithmTbb(const std::vector<Point>& points) {
     if (count_points < 2)
         return points;
 
-    FirstPoint base_point(points, points[0]);
-    tbb::parallel_reduce(tbb::blocked_range<size_t>(1, count_points), base_point);
-    Point base = base_point.getBase();
+    Point base = tbb::parallel_reduce(
+        tbb::blocked_range<size_t>(1, count_points),
+        points[0],
+        [&] (tbb::blocked_range<size_t> r, Point local_base) -> Point {
+            auto begin = r.begin(), end = r.end();
+            for (auto i = begin; i != end; i++) {
+                if (points[i] < local_base)
+                    local_base = points[i];
+            }
+            return local_base;
+        },
+        [] (const Point a, const Point b) {
+            return a < b ? a : b;
+        });
 
     std::vector<Point> convex_hull;
     convex_hull.push_back(base);
@@ -167,9 +104,34 @@ std::vector<Point> jarvisAlgorithmTbb(const std::vector<Point>& points) {
     Point next;
     do {
         next = current == points[0] ? points[1] : points[0];
-        NextPoint next_point(points, next, current);
-        tbb::parallel_reduce(tbb::blocked_range<size_t>(0, count_points), next_point);
-        current = next_point.getNext();
+
+        current = tbb::parallel_reduce(
+            tbb::blocked_range<size_t>(0, count_points),
+            next,
+            [&] (tbb::blocked_range<size_t> r, Point local_next) -> Point {
+                auto begin = r.begin(), end = r.end();
+                for (auto i = begin; i != end; i++) {
+                    int temp = rotate(current, local_next, points[i]);
+                    if (temp > 0) {
+                        local_next = points[i];
+                    } else if (temp == 0) {
+                        if (checkPointsDistances(current, local_next, points[i]))
+                            local_next = points[i];
+                    }
+                }
+                return local_next;
+            },
+            [&] (const Point next, const Point local_next) {
+                int temp = rotate(current, next, local_next);
+                if (temp > 0) {
+                    return local_next;
+                } else if (temp == 0) {
+                    if (checkPointsDistances(current, next, local_next))
+                        return local_next;
+                }
+                return next;
+            });
+
         next = current;
         convex_hull.push_back(next);
     } while (current != base);
