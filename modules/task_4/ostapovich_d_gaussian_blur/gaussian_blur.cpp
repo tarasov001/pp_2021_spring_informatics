c7462f81871138c99be6ce2eba9efbb37d0611e4
Denis Ostapovich <vofksden@gmail.com>
2021-04-10 19:52:36 +0300

    dev

--- modules/task_4/ostapovich_d_gaussian_blur/gaussian_blur.cpp
+++ modules/task_4/ostapovich_d_gaussian_blur/gaussian_blur.cpp
@@ -6,6 +6,8 @@
 #include <stdexcept>
 #include <random>
 #include "../../../modules/task_4/ostapovich_d_gaussian_blur/gaussian_blur.h"
+#include "../../../3rdparty/unapproved/unapproved.h"
+#include <iostream>
 
 std::vector<uint8_t> filter(const std::vector<uint8_t>& matrix, int width, int coreSize) {
     std::size_t length = matrix.size();
@@ -16,8 +18,26 @@ std::vector<uint8_t> filter(const std::vector<uint8_t>& matrix, int width, int c
 
     int radius = coreSize / 2;
     auto core = calculateCore(coreSize);
+
+    std::cout << "Core:\n";
+    for (int i = 0; i < core.size(); i++) {
+        std::cout << +core[i] << " ";
+        if (i % width == width - 1) {
+            std::cout << std::endl;
+        }
+    }
+
     int height = length / width;
-    std::vector<uint8_t> filtered(width * height);
+    std::vector<uint8_t> filtered(matrix);
+
+    std::cout << "Filtered:\n";
+    for (int i = 0; i < filtered.size(); i++) {
+        std::cout << +filtered[i] << " ";
+        if (i % width == width - 1) {
+            std::cout << std::endl;
+        }
+    }
+
     for (int i = radius; i < height - radius; i++) {
         for (int j = radius; j < width - radius; j++) {
             uint8_t result = 0;
@@ -34,18 +54,103 @@ std::vector<uint8_t> filter(const std::vector<uint8_t>& matrix, int width, int c
 }
 
 std::vector<uint8_t> filterParallel(const std::vector<uint8_t>& matrix, int width, int coreSize) {
-    std::size_t length = matrix.size();
+    const std::size_t length = matrix.size();
+    //const int nthreads = std::thread::hardware_concurrency();
+    const int nthreads = 2;
 
     if (length == 0 || width <= 0 || length % width != 0 || coreSize < 3 || coreSize % 2 == 0) {
         throw std::invalid_argument("Zero or negative argument passed");
     }
 
-    int radius = coreSize / 2;
-    std::vector<double> core = calculateCore(coreSize);
-    int height = length / width;
-    std::vector<uint8_t> filtered(width * height);
-	
-	return filtered;
+    const int radius = coreSize / 2;
+    const std::vector<double> core = calculateCore(coreSize);
+
+    std::cout << "Core:\n";
+    for (int i = 0; i < core.size(); i++) {
+        std::cout << +core[i] << " ";
+        if (i % width == width - 1) {
+            std::cout << std::endl;
+        }
+    }
+
+    const int height = length / width;
+    std::vector<uint8_t> filtered(matrix);
+
+    std::cout << "Filtered:\n";
+    for (int i = 0; i < filtered.size(); i++) {
+        std::cout << +filtered[i] << " ";
+        if (i % width == width - 1) {
+            std::cout << std::endl;
+        }
+    }
+
+    const int rowIterations = height - 2 * radius;
+    const int columnIterations = width - 2 * radius;
+
+    const int rowDelta = rowIterations / nthreads;
+    const int rowRemainder = rowIterations % nthreads;
+    const int columnDelta = columnIterations / nthreads;
+    const int columnRemainder = columnIterations % nthreads;
+
+    if (rowDelta == 0 && columnDelta == 0) {
+        return filter(matrix, width, coreSize);
+    }
+
+    std::thread* threads = new std::thread[nthreads];
+
+    for (int i = 0; i < nthreads; i++) {
+
+        /*int startRow = radius + i * rowDelta;
+        int endRow = i == nthreads - 1 ? startRow + rowDelta + rowRemainder - 1: startRow + rowDelta - 1;
+        int startColumn = radius + i * columnDelta;
+        int endColumn = i == nthreads - 1 ?
+            startColumn + columnDelta + rowRemainder - 1 : startColumn + columnDelta - 1;*/
+
+        /*threads[i] = std::thread([&](int startRow, int endRow,
+            int startColumn, int endColumn) {
+                for (int j = startRow; j <= endRow; j++) {
+                    for (int k = startColumn; k <= endColumn; k++) {
+                    uint8_t result = 0;
+                    for (int m = -radius; m <= radius; m++) {
+                        for (int n = -radius; n <= radius; n++) {
+                            result += matrix[width * (j + m) + k + n] * core[coreSize * (m + radius) + n + radius];
+                        }
+                    }
+
+                    filtered[width * j + k] = result;
+                }
+            }
+            }, startRow, endRow,
+                startColumn, endColumn);*/
+        threads[i] = std::thread([&](int rowDelta, int rowRemainder,
+            int columnDelta, int columnRemainder) {
+                for (int j = radius; j < height - radius; j++) {
+                    for (int k = radius; k < width - radius; k++) {
+                        uint8_t result = 0;
+                        int a = k - radius >= columnDelta * (nthreads - 1) ? nthreads - 1 : (k - radius) / columnDelta;
+                        int b = j - radius >= rowDelta * (nthreads - 1) ? nthreads - 1 : (j - radius) / rowDelta;
+                        if ((a + b)%nthreads == i) {
+                            for (int m = -radius; m <= radius; m++) {
+                                for (int n = -radius; n <= radius; n++) {
+                                    result += matrix[width * (j + m) + k + n] * core[coreSize * (m + radius) + n + radius];
+                                }
+                            }
+                        }
+
+                        filtered[width * j + k] = result;
+                    }
+                }
+            }, rowDelta, rowRemainder,
+            columnDelta, columnRemainder);
+    }
+    
+    for (int i = 0; i < nthreads; i++) {
+        threads[i].join();
+    }
+
+    delete[]threads;
+
+    return filtered;
 }
 
 std::vector<double> calculateCore(int size, double deviation) {
--- modules/task_4/ostapovich_d_gaussian_blur/main.cpp
+++ modules/task_4/ostapovich_d_gaussian_blur/main.cpp
@@ -1,119 +1,141 @@
 // Copyright 2021 Denis Ostapovich
 #include <gtest/gtest.h>
 #include <vector>
+#include <chrono>
 #include "./gaussian_blur.h"
 
-TEST(TBB_Gaussian_blur, GetRandomMatrix_Throws_On_Zero_Args) {
-    ASSERT_ANY_THROW(getRandomMatrix(0, 0));
-}
-
-TEST(TBB_Gaussian_blur, GetRandomMatrix_Throws_On_Negative_Args) {
-    ASSERT_ANY_THROW(getRandomMatrix(-1, -1));
-}
-
-TEST(TBB_Gaussian_blur, GetRandomMatrix_No_Throw_On_Valid_Args) {
-    ASSERT_NO_THROW(getRandomMatrix(3, 5));
-}
-
-TEST(TBB_Gaussian_blur, GetRandomMatrix_Returns_Vector_Of_Valid_Size) {
-    auto vec = getRandomMatrix(2, 7);
-    ASSERT_EQ(vec.size(), (size_t)14);
-}
-
-TEST(TBB_Gaussian_blur, CalculateCore_Returns_Valid_Core) {
-    int size = 5;
-    auto core = calculateCore(size);
-    for (double num : core) {
-        ASSERT_NE(num, 0);
-    }
-}
-
-TEST(TBB_Gaussian_blur, Filter_Throws_On_Zero_Size_Matrix) {
-    std::vector<uint8_t> mat;
-    ASSERT_ANY_THROW(filter(mat, 10));
-}
-
-TEST(TBB_Gaussian_blur, Filter_Throws_On_Zero_Or_Negative_Width_Value) {
-    auto mat = getRandomMatrix(11, 21);
-    ASSERT_ANY_THROW(filter(mat, 0));
-    ASSERT_ANY_THROW(filter(mat, -1));
-}
-
-TEST(TBB_Gaussian_blur, Filter_Throws_Invalid_Width_Value) {
-    auto mat = getRandomMatrix(11, 21);
-    ASSERT_ANY_THROW(filter(mat, 10));
-}
-
-TEST(TBB_Gaussian_blur, Filter_Throws_On_Invalid_Core_Size) {
-    int width = 11;
-    auto mat = getRandomMatrix(11, 21);
-    ASSERT_ANY_THROW(filter(mat, width, 0));
-    ASSERT_ANY_THROW(filter(mat, width, -1));
-    ASSERT_ANY_THROW(filter(mat, width, 4));
-}
-
-TEST(TBB_Gaussian_blur, Filter_No_Throw_On_Valid_Args) {
-    int width = 21;
-    int coreSize = 3;
-    auto mat = getRandomMatrix(width, 31);
-    ASSERT_NO_THROW(filter(mat, width, coreSize));
-}
-
-TEST(TBB_Gaussian_blur, FilterParallel_Throws_On_Zero_Size_Matrix) {
-    std::vector<uint8_t> mat;
-    ASSERT_ANY_THROW(filterParallel(mat, 10));
-}
-
-TEST(TBB_Gaussian_blur, FilterParallel_Throws_On_Zero_Or_Negative_Width_Value) {
-    auto mat = getRandomMatrix(11, 21);
-    ASSERT_ANY_THROW(filterParallel(mat, 0));
-    ASSERT_ANY_THROW(filterParallel(mat, -1));
-}
-
-TEST(TBB_Gaussian_blur, FilterParallel_Throws_Invalid_Width_Value) {
-    auto mat = getRandomMatrix(10, 20);
-    ASSERT_ANY_THROW(filterParallel(mat, 3));
-}
-
-TEST(TBB_Gaussian_blur, FilterParallel_Throws_On_Invalid_Core_Size) {
-    auto mat = getRandomMatrix(11, 21);
-    ASSERT_ANY_THROW(filterParallel(mat, 11, 0));
-    ASSERT_ANY_THROW(filterParallel(mat, 11, -5));
-    ASSERT_ANY_THROW(filterParallel(mat, 11, 8));
-}
-
-TEST(TBB_Gaussian_blur, FilterParallel_No_Throw_On_Valid_Args) {
-    auto mat = getRandomMatrix(10, 20);
-    ASSERT_NO_THROW(filterParallel(mat, 10));
-}
+//TEST(TBB_Gaussian_blur, GetRandomMatrix_Throws_On_Zero_Args) {
+//    ASSERT_ANY_THROW(getRandomMatrix(0, 0));
+//}
+//
+//TEST(TBB_Gaussian_blur, GetRandomMatrix_Throws_On_Negative_Args) {
+//    ASSERT_ANY_THROW(getRandomMatrix(-1, -1));
+//}
+//
+//TEST(TBB_Gaussian_blur, GetRandomMatrix_No_Throw_On_Valid_Args) {
+//    ASSERT_NO_THROW(getRandomMatrix(3, 5));
+//}
+//
+//TEST(TBB_Gaussian_blur, GetRandomMatrix_Returns_Vector_Of_Valid_Size) {
+//    auto vec = getRandomMatrix(2, 7);
+//    ASSERT_EQ(vec.size(), (size_t)14);
+//}
+//
+//TEST(TBB_Gaussian_blur, CalculateCore_Returns_Valid_Core) {
+//    int size = 5;
+//    auto core = calculateCore(size);
+//    for (double num : core) {
+//        ASSERT_NE(num, 0);
+//    }
+//}
+//
+//TEST(TBB_Gaussian_blur, Filter_Throws_On_Zero_Size_Matrix) {
+//    std::vector<uint8_t> mat;
+//    ASSERT_ANY_THROW(filter(mat, 10));
+//}
+//
+//TEST(TBB_Gaussian_blur, Filter_Throws_On_Zero_Or_Negative_Width_Value) {
+//    auto mat = getRandomMatrix(11, 21);
+//    ASSERT_ANY_THROW(filter(mat, 0));
+//    ASSERT_ANY_THROW(filter(mat, -1));
+//}
+//
+//TEST(TBB_Gaussian_blur, Filter_Throws_Invalid_Width_Value) {
+//    auto mat = getRandomMatrix(11, 21);
+//    ASSERT_ANY_THROW(filter(mat, 10));
+//}
+//
+//TEST(TBB_Gaussian_blur, Filter_Throws_On_Invalid_Core_Size) {
+//    int width = 11;
+//    auto mat = getRandomMatrix(11, 21);
+//    ASSERT_ANY_THROW(filter(mat, width, 0));
+//    ASSERT_ANY_THROW(filter(mat, width, -1));
+//    ASSERT_ANY_THROW(filter(mat, width, 4));
+//}
+//
+//TEST(TBB_Gaussian_blur, Filter_No_Throw_On_Valid_Args) {
+//    int width = 21;
+//    int coreSize = 3;
+//    auto mat = getRandomMatrix(width, 31);
+//    ASSERT_NO_THROW(filter(mat, width, coreSize));
+//}
+//
+//TEST(TBB_Gaussian_blur, FilterParallel_Throws_On_Zero_Size_Matrix) {
+//    std::vector<uint8_t> mat;
+//    ASSERT_ANY_THROW(filterParallel(mat, 10));
+//}
+//
+//TEST(TBB_Gaussian_blur, FilterParallel_Throws_On_Zero_Or_Negative_Width_Value) {
+//    auto mat = getRandomMatrix(11, 21);
+//    ASSERT_ANY_THROW(filterParallel(mat, 0));
+//    ASSERT_ANY_THROW(filterParallel(mat, -1));
+//}
+//
+//TEST(TBB_Gaussian_blur, FilterParallel_Throws_Invalid_Width_Value) {
+//    auto mat = getRandomMatrix(10, 20);
+//    ASSERT_ANY_THROW(filterParallel(mat, 3));
+//}
+//
+//TEST(TBB_Gaussian_blur, FilterParallel_Throws_On_Invalid_Core_Size) {
+//    auto mat = getRandomMatrix(11, 21);
+//    ASSERT_ANY_THROW(filterParallel(mat, 11, 0));
+//    ASSERT_ANY_THROW(filterParallel(mat, 11, -5));
+//    ASSERT_ANY_THROW(filterParallel(mat, 11, 8));
+//}
+//
+//TEST(TBB_Gaussian_blur, FilterParallel_No_Throw_On_Valid_Args) {
+//    auto mat = getRandomMatrix(10, 20);
+//    ASSERT_NO_THROW(filterParallel(mat, 10));
+//}
 
 TEST(TBB_Gaussian_blur, FilterParallel_Result_Equals_To_Filter_Result) {
-    int width = 31;
-    int coreSize = 5;
-    auto mat = getRandomMatrix(width, 44);
+    const int width = 5;
+    const int height = 5;
+    const int coreSize = 3;
+    auto mat = getRandomMatrix(width, height);
+
     auto expected = filter(mat, width, coreSize);
     auto calculated = filterParallel(mat, width, coreSize);
+
+    std::cout << "Expected:\n";
+    for (int i = 0; i < expected.size(); i++) {
+        std::cout << +expected[i] << " ";
+        if (i % width == width - 1) {
+            std::cout << std::endl;
+        }
+    }
+
+    std::cout << std::endl;
+
+    std::cout << "Calculated:\n";
+    for (int i = 0; i < calculated.size(); i++) {
+        std::cout << +calculated[i] << " ";
+        if (i % width == width - 1) {
+            std::cout << std::endl;
+        }
+    }
+
+    std::cout << std::endl;
+
     ASSERT_EQ(expected, calculated);
 }
 
-// TEST(TBB_Gaussian_blur, FilterParallel_Accelerates) {
-//    double sequential, parallel;
+//TEST(TBB_Gaussian_blur, FilterParallel_Accelerates) {
 //    int width = 1000, height = 1000;
 //    auto mat = getRandomMatrix(width, height);
-
-//    double start, end;
-//    start = omp_get_wtime();
+//
+//    auto start = std::chrono::high_resolution_clock::now();
 //    filter(mat, width, 9);
-//    end = omp_get_wtime();
-//    sequential = end - start;
-
-//    start = omp_get_wtime();
+//    auto end = std::chrono::high_resolution_clock::now();
+//    auto sequential = end - start;
+//
+//    start = std::chrono::high_resolution_clock::now();
 //    filterParallel(mat, width, 9);
-//    end = omp_get_wtime();
-//    parallel = end - start;
-
+//    end = std::chrono::high_resolution_clock::now();
+//    auto parallel = end - start;
+//
 //    ASSERT_GT(sequential, parallel);
-//    std::cout << "Sequential: " << sequential << std::endl;
-//    std::cout << "Parallel: " << parallel << std::endl;
+//    std::cout << "Sequential: " << sequential.count() << std::endl;
+//    std::cout << "Parallel: " << parallel.count() << std::endl;
 //    std::cout << "Scale: " << sequential / parallel << std::endl;
-// }
+//}
