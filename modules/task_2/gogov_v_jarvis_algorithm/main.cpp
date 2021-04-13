70741a8681543e6ac80d2fffe8dd3ee51005984f
vlad-gogov <43129455+vlad-gogov@users.noreply.github.com>
2021-04-13 14:17:30 +0300

    main

--- modules/task_2/gogov_v_jarvis_algorithm/main.cpp
+++ modules/task_2/gogov_v_jarvis_algorithm/main.cpp
@@ -29,90 +29,33 @@ TEST(OpenMP_Jarvis_Algorithm, Jarvis_Algorithm_Can_Apply_With_One_Line_Points) {
 }
 
 TEST(OpenMP_Jarvis_Algorithm, Jarvis_Algorithm_Square_With_Points) {
-    std::vector<Point> points, convex_hull_seq, convex_hull_omp;
-    try {
-    points = {Point(0, 0),   Point(25, 25), Point(34, 10),
-              Point(0, 40),  Point(10, 2),  Point(40, 40),
-              Point(15, 15), Point(40, 0),  Point(5, 5)};
-    } catch (const std::bad_alloc& e) {
-        std::cout << "Points. " << e.what() << std::endl;
-    }
-    try {
-       convex_hull_seq = jarvisAlgorithmSeq(points);
-    } catch (const std::bad_alloc& e) {
-        std::cout << "Seq. " << e.what() << std::endl;
-    }
-    try {
-            convex_hull_omp = jarvisAlgorithmOmp(points);
-    } catch (const std::bad_alloc& e) {
-        std::cout << "Omp. " << e.what() << std::endl;
-    }
+    std::vector<Point> points = {Point(0, 0),   Point(25, 25), Point(34, 10),
+                                 Point(0, 40),  Point(10, 2),  Point(40, 40),
+                                 Point(15, 15), Point(40, 0),  Point(5, 5)};
+    std::vector<Point> convex_hull_seq = jarvisAlgorithmSeq(points);
+    std::vector<Point> convex_hull_omp = jarvisAlgorithmOmp(points);
     ASSERT_EQ(convex_hull_seq, convex_hull_omp);
 }
 
 TEST(OpenMP_Jarvis_Algorithm, Jarvis_Algorithm_Can_Apply_All_Point_Convex_Hull) {
-    std::vector<Point> points, convex_hull_seq, convex_hull_omp;
-    try {
-    points = {Point(2, 5), Point(50, 10), Point(80, 30), Point(50, 50), Point(7, 25)};
-    } catch (const std::bad_alloc& e) {
-        std::cout << "Points. " << e.what() << std::endl;
-    }
-    try {
-       convex_hull_seq = jarvisAlgorithmSeq(points);
-    } catch (const std::bad_alloc& e) {
-        std::cout << "Seq. " << e.what() << std::endl;
-    }
-    try {
-            convex_hull_omp = jarvisAlgorithmOmp(points);
-    } catch (const std::bad_alloc& e) {
-        std::cout << "Omp. " << e.what() << std::endl;
-    }
+    std::vector<Point> points = {Point(2, 5), Point(50, 10), Point(80, 30), Point(50, 50), Point(7, 25)};
+    std::vector<Point> convex_hull_seq = jarvisAlgorithmSeq(points);
+    std::vector<Point> convex_hull_omp = jarvisAlgorithmOmp(points);
     ASSERT_EQ(convex_hull_seq, convex_hull_omp);
 }
 
 TEST(OpenMP_Jarvis_Algorithm, Jarvis_Algorithm_With_Size_102) {
     size_t count_point = 102ull;
-        std::vector<Point> points, convex_hull_seq, convex_hull_omp;
-    try {
-    points = getRandomPoints(count_point);
-    } catch (const std::bad_alloc& e) {
-        std::cout << "Points. " << e.what() << std::endl;
-    }
-    try {
-       convex_hull_seq = jarvisAlgorithmSeq(points);
-    } catch (const std::bad_alloc& e) {
-        std::cout << "Seq. " << e.what() << std::endl;
-    }
-    try {
-            convex_hull_omp = jarvisAlgorithmOmp(points);
-    } catch (const std::bad_alloc& e) {
-        std::cout << "Omp. " << e.what() << std::endl;
-    }
-    ASSERT_EQ(convex_hull_seq, convex_hull_omp);
+    std::vector<Point> points = getRandomPoints(count_point);
+    std::vector<Point> convex_hul_seq = jarvisAlgorithmSeq(points);
+    std::vector<Point> convex_hul_omp = jarvisAlgorithmOmp(points);
+    ASSERT_EQ(convex_hul_seq, convex_hul_omp);
 }
 
 TEST(OpenMP_Jarvis_Algorithm, Jarvis_Algorithm_With_Size_111) {
     size_t count_point = 111ull;
-        std::vector<Point> points, convex_hull_seq, convex_hull_omp;
-    try {
-    points = getRandomPoints(count_point);
-    } catch (const std::bad_alloc& e) {
-        std::cout << "Points. " << e.what() << std::endl;
-    }
-    try {
-       convex_hull_seq = jarvisAlgorithmSeq(points);
-    } catch (const std::bad_alloc& e) {
-        std::cout << "Seq. " << e.what() << std::endl;
-    }
-        try {
-            convex_hull_omp = jarvisAlgorithmOmp(points);
-    } catch (const std::bad_alloc& e) {
-        std::cout << "Omp. " << e.what() << std::endl;
-    }
-    ASSERT_EQ(convex_hull_seq, convex_hull_omp);
-}
-
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    std::vector<Point> points = getRandomPoints(count_point);
+    std::vector<Point> convex_hul_seq = jarvisAlgorithmSeq(points);
+    std::vector<Point> convex_hul_omp = jarvisAlgorithmOmp(points);
+    ASSERT_EQ(convex_hul_seq, convex_hul_omp);
 }
