35ead47fd076d34656c200ebf25ffbb9bb07e5bd
RustamovAzer <43135865+RustamovAzer@users.noreply.github.com>
2021-04-14 19:11:40 +0300

    get rid of outputs in tests

--- modules/task_2/rustamov_a_histogram_stretch/main.cpp
+++ modules/task_2/rustamov_a_histogram_stretch/main.cpp
@@ -4,72 +4,30 @@
 #include <iostream>
 #include "../../../modules/task_2/rustamov_a_histogram_stretch/histogram_stretch.h"
 
-// #include "histogram_stretch.h"
-
-
-
-TEST(Histogram_Stretch, Generate_Image_Parallel_1000x1000) {
-    int w = 1000, h = 1000;
-    double time1, time2, time3;
-    time1 = omp_get_wtime();
-    Matrix image_seq = generate_random_image(w, h);
-    time2 = omp_get_wtime();
-    Matrix image_omp = generate_random_image_parallel(w, h);
-    time3 = omp_get_wtime();
-    std::cout << "TOTAL TIME: " << (time3 - time1) << std::endl <<
-                "1: GENERATE RANDOM IMAGE SEQUENTIAL: " << time2 - time1 << std::endl <<
-                "2: GENERATE RANDOM IMAGE PARALLEL: " << time3 - time2 << std::endl;
-    SUCCEED();
-}
-
 TEST(Histogram_Stretch, Correct_Make_Histogram_Parallel) {
-    double time1, time2, time3, time4;
     int w = 500, h = 500;
-    time1 = omp_get_wtime();
+
     Matrix image = generate_random_image_parallel(w, h);
-    time2 = omp_get_wtime();
     Matrix hist_seq = make_histogram(image, w, h);
-    time3 = omp_get_wtime();
     Matrix hist_omp = make_histogram_parallel(image, w, h);
-    time4 = omp_get_wtime();
-    std::cout << "TOTAL TIME: " << (time4 - time1) << std::endl <<
-                "1: GENERATE RANDOM IMAGE PARALLEL: " << time2 - time1 << std::endl <<
-                "2: HISTOGRAM SEQUENTIAL: " << time3 - time2 << std::endl <<
-                "3: HISTOGRAM PARALLEL: " << time4 - time3 << std::endl;
+
     for (int i = 0; i < 256; i++) {
         ASSERT_EQ(hist_seq[i], hist_omp[i]);
     }
 }
 
 TEST(Histogram_Stretch, Correct_Stretch_histogram_Parallel) {
-    double time1, time2, time3, time4, time5, time6;
     int min_y, max_y;
     int w = 500, h = 500;
     Matrix str_hist_seq(256), str_hist_omp(256);
-    time1 = omp_get_wtime();
 
     Matrix image = generate_random_image_parallel(w, h);
-    time2 = omp_get_wtime();
-
     Matrix hist_omp = make_histogram_parallel(image, w, h);
-    time3 = omp_get_wtime();
-
     min_y = get_min_y(hist_omp);
     max_y = get_max_y(hist_omp);
 
-    time4 = omp_get_wtime();
     Matrix res_seq = increase_contrast(image, w, h, min_y, max_y);
-    time5 = omp_get_wtime();
-
     Matrix res_omp = increase_contrast_parallel(image, w, h, min_y, max_y);
-    time6 = omp_get_wtime();
-
-    std::cout << "TOTAL TIME: " << time6 - time1 << std::endl <<
-                "1: GENERATE RANDOM IMAGE PARALLEL: " << time2 - time1 << std::endl <<
-                "2: MAKE HISTOGRAM: " << time3 - time2 << std::endl <<
-                "3: FIND MIN_Y AND MAX_Y: " << time4 - time3 << std::endl <<
-                "4: INCREASE CONTRAST SEQUENTIAL: " << time5 - time4 << std::endl <<
-                "5: INCREASE CONTRAST PARALLEL: " << time6 - time5 << std::endl;
 
     for (int i = 0; i < h * w; i++) {
         ASSERT_EQ(res_seq[i], res_omp[i]);
@@ -78,30 +36,17 @@ TEST(Histogram_Stretch, Correct_Stretch_histogram_Parallel) {
 
 TEST(Histogram_Stretch, Correct_Histogram_stretch_Algorithm) {
     int w = 5000, h = 5000;
-    double time1, time2, time3, time4;
-    time1 = omp_get_wtime();
 
     Matrix image = generate_random_image_parallel(w, h);
-    time2 = omp_get_wtime();
-
     Matrix result_seq = histogram_sretch_algorithm(image, w, h);
-    time3 = omp_get_wtime();
-
     Matrix result_omp = histogram_sretch_algorithm_parallel(image, w, h);
-    time4 = omp_get_wtime();
-
-    std::cout << "TOTAL TIME: " << time4 - time1 << std::endl <<
-                "1: GENERATE RANDOM IMAGE PARALLEL: " << time2 - time1 << std::endl <<
-                "2: HISTOGRAM STRETCH ALGORITHM SEQUENTIAL: " << time3 - time2 << std::endl <<
-                "3: HISTOGRAM STRETCH ALGORITHM PARALLEL: " << time4 - time3 << std::endl <<
-                "4: COMPARATIVE ACCELERATION: " << (time3 - time2) / (time4 - time3) << std::endl;
 
     for (int i = 0; i < h * w; i++) {
         ASSERT_EQ(result_seq[i], result_omp[i]);
     }
 }
 
-/*
+
 TEST(Histogram_Stretch, Incorrect_Image) {
     int w = 0, h = 10;
     ASSERT_ANY_THROW(generate_random_image(w, h));
@@ -174,7 +119,7 @@ TEST(Histogram_Stretch, Cannot_Stretch_Twice) {
         ASSERT_EQ(result[i], second_result[i]);
     }
 }
-*/
+
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
