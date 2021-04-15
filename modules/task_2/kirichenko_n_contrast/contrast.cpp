e066821ca6b300a891f8788680a53a79643e19d5
Nikita23456 <kirichenko-nikita2000@yandex.ru>
2021-04-15 23:46:12 +0300

    v1

---
+++ modules/task_2/kirichenko_n_contrast/contrast.cpp
@@ -0,0 +1,61 @@
+// Copyright 2021 Kirichenko Nikita
+#include "../../../modules/task_2/kirichenko_n_contrast/contrast.h"
+
+Result RandomM(int l, int k) {
+    if (l <= 0 || k <= 0)
+        throw std::runtime_error("Incorrect data!");
+    static std::mt19937 random(time(0));
+    Result res(l * k);
+    for (int i = 0; i < l; i++) {
+        for (int j = 0; j < k; j++) {
+            res[i * l + j] = random() % 256;
+        }
+    }
+    return res;
+}
+
+Result Contrast(const Result& rm) {
+    Result outcome(rm.size());
+    int max = 0, min = 255;
+    for (int i = 0; i < static_cast<int>(rm.size()); i++) {
+        if (min > rm[i])
+            min = rm[i];
+        if (max < rm[i])
+            max = rm[i];
+    }
+    for (int i = 0; i < static_cast<int>(rm.size()); i++) {
+        outcome[i] = (rm[i] - min) * (255 / (max - min));
+    }
+    return outcome;
+}
+
+Result Contrastomp(const Result& rm) {
+    Result outcome(rm.size());
+    int const numThreads = 3;
+    omp_set_num_threads(numThreads);
+    int min_n[numThreads], max_n[numThreads];
+    int max = 0, min = 255;
+#pragma omp parallel 
+    {
+       int nT = omp_get_thread_num();
+        min_n[nT] = 255;
+        max_n[nT] = 0;
+#pragma omp for schedule (static)
+        for (int i = 0; i < static_cast<int>(rm.size()); i++) {
+            if (min_n[nT] > rm[i])
+                min_n[nT] = rm[i];
+            if (max_n[nT] < rm[i])
+                max_n[nT] = rm[i];
+        }
+    }
+    for (int i = 0; i < numThreads; i++) {
+        if (min_n[i] < min)
+            min = min_n[i];
+        if (max_n[i] > max)
+            max = max_n[i];
+    }
+    for (int i = 0; i < static_cast<int>(rm.size()); i++) {
+         outcome[i] = (rm[i] - min) * (255 / (max - min));
+    }
+    return outcome;
+}
---
+++ modules/task_2/kirichenko_n_contrast/contrast.h
@@ -0,0 +1,19 @@
+// Copyright 2021 Kirichenko Nikita
+#ifndef MODULES_TASK_2_KIRICHENKO_N_CONTRAST_CONTRAST_H_
+#define MODULES_TASK_2_KIRICHENKO_N_CONTRAST_CONTRAST_H_
+
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <random>
+#include <ctime>
+#include <algorithm>
+#include <omp.h>
+
+using Result = std::vector<int>;
+
+Result RandomM(int l, int k);
+Result Contrast(const Result& rm);
+Result Contrastomp(const Result& rm);
+
+#endif  // MODULES_TASK_2_KIRICHENKO_N_CONTRAST_CONTRAST_H_
