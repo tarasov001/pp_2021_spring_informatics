b41372fcf707dca78e2ce6c2e31d80d633a85ac9
Elizaveta Prokofeva <prokof.15@mail.ru>
2021-04-15 09:50:47 +0300

    fix linter 2

--- modules/task_2/prokofeva_e_sparse_crs_omp/main.cpp
+++ modules/task_2/prokofeva_e_sparse_crs_omp/main.cpp
@@ -11,7 +11,6 @@ TEST(Sparse_crs_matrix, Test1) {
     crs_matrix second = generate(50);
     crs_matrix res = mult(first, second);
     crs_matrix resp = parallel_mult(first, second);
-
     ASSERT_EQ(resp.values, res.values);
     ASSERT_EQ(resp.cols, res.cols);
     ASSERT_EQ(resp.row_index, res.row_index);
@@ -21,7 +20,6 @@ TEST(Sparse_crs_matrix, Test2) {
     crs_matrix second = generate(100);
     crs_matrix res = mult(first, second);
     crs_matrix resp = parallel_mult(first, second);
-
     ASSERT_EQ(resp.values, res.values);
     ASSERT_EQ(resp.cols, res.cols);
     ASSERT_EQ(resp.row_index, res.row_index);
@@ -31,7 +29,6 @@ TEST(Sparse_crs_matrix, Test3) {
     crs_matrix second = generate(200);
     crs_matrix res = mult(first, second);
     crs_matrix resp = parallel_mult(first, second);
-
     ASSERT_EQ(resp.values, res.values);
     ASSERT_EQ(resp.cols, res.cols);
     ASSERT_EQ(resp.row_index, res.row_index);
@@ -41,7 +38,6 @@ TEST(Sparse_crs_matrix, Test4) {
     crs_matrix second = generate(350);
     crs_matrix res = mult(first, second);
     crs_matrix resp = parallel_mult(first, second);
-    
     ASSERT_EQ(resp.values, res.values);
     ASSERT_EQ(resp.cols, res.cols);
     ASSERT_EQ(resp.row_index, res.row_index);
@@ -51,7 +47,6 @@ TEST(Sparse_crs_matrix, Test5) {
     crs_matrix second = generate(500);
     crs_matrix res = mult(first, second);
     crs_matrix resp = parallel_mult(first, second);
-    
     ASSERT_EQ(resp.values, res.values);
     ASSERT_EQ(resp.cols, res.cols);
     ASSERT_EQ(resp.row_index, res.row_index);
