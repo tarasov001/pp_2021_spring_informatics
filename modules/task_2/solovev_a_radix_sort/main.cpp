8cc73c7c083c77d4cd69eabce276fb90b70be2fa
Alexandr-Solovev <n19ht1n9ale27@gmail.com>
2021-04-17 16:18:48 +0300

    first commit

---
+++ modules/task_2/solovev_a_radix_sort/main.cpp
@@ -0,0 +1,136 @@
+// Copyright 2021 Solovev Aleksandr
+#include <numeric>
+#include <utility>
+#include <algorithm>
+#include "gtest/gtest.h"
+#include "../../../modules/task_2/solovev_a_radix_sort/radix_sort.h"
+
+
+TEST(Seqvspar, Test_Only_Positive) {
+    const int length = 100;
+    int statusseq = 0;
+    int statuspar = 0;
+    std::vector<int> buffer1(length);
+    statusseq = generateRandomArray(&buffer1, -100, 100);
+    std::vector<int> buffer2 = buffer1;
+    statusseq = RadixSort(&buffer1);
+    statusseq = SortingCheck(&buffer1);
+    statuspar = ParallelSorting(&buffer2);
+    statuspar = SortingCheck(&buffer2);
+    
+    ASSERT_EQ(buffer2, buffer1);
+}
+
+
+TEST(Senquential, Test_Only_Negative) {
+    const int length = 10;
+    int statusseq = 0;
+    int statuspar = 0;
+    std::vector<int> buffer1(length);
+    statusseq = generateRandomArray(&buffer1, -1000, 1000);
+    std::vector<int> buffer2 = buffer1;
+    statusseq = RadixSort(&buffer1);
+    statusseq = SortingCheck(&buffer1);
+    statuspar = ParallelSorting(&buffer2);
+    statuspar = SortingCheck(&buffer2);
+
+    ASSERT_EQ(statusseq, statuspar);
+}
+
+TEST(Senquential, Test_Small_length) {
+    const int length = 100000;
+    int statusseq = 0;
+    int statuspar = 0;
+    std::vector<int> buffer1(length);
+    statusseq = generateRandomArray(&buffer1, -10000, 10000);
+
+    std::vector<int> buffer2 = buffer1;
+    statusseq = RadixSort(&buffer1);
+    statusseq = SortingCheck(&buffer1);
+    statuspar = ParallelSorting(&buffer2);
+    statuspar = SortingCheck(&buffer2);
+    
+    ASSERT_EQ(statusseq, statuspar);
+}
+
+TEST(Senquential, Test_Medium_Length) {
+    const int length = 100000;
+    int statusseq = 0;
+    int statuspar = 0;
+    std::vector<int> buffer1(length);
+    statusseq = generateRandomArray(&buffer1,  -1000, 1000);
+    std::vector<int> buffer2 = buffer1;
+    statusseq = RadixSort(&buffer1);
+    statusseq = SortingCheck(&buffer1);
+
+    statuspar = ParallelSorting(&buffer2);
+    statuspar = SortingCheck(&buffer2);
+
+    
+    ASSERT_EQ(statusseq, statuspar);
+}
+
+TEST(Senquential, Test_Large_Length) {
+    const int length = 100000;
+    int statusseq = 0;
+    int statuspar = 0;
+    std::vector<int> buffer1(length);
+    statusseq = generateRandomArray(&buffer1,  -1000, 1000);
+    std::vector<int> buffer2 = buffer1;
+
+    statusseq = RadixSort(&buffer1);
+    statusseq = SortingCheck(&buffer1);
+    
+    statuspar = ParallelSorting(&buffer2);
+    statuspar = SortingCheck(&buffer2);
+    
+    
+    ASSERT_EQ(statusseq, statuspar);
+}
+
+TEST(Senquential, Test_Very_Large_Length) {
+    const int length = 10000;
+    int statusseq = 0;
+    int statuspar = 0;
+    std::vector<int> buffer1(length);
+    statusseq = generateRandomArray(&buffer1,  -5000, 5000);
+    std::vector<int> buffer2 = buffer1;
+    statusseq = RadixSort(&buffer1);
+    statusseq = SortingCheck(&buffer1);
+    
+    statuspar = ParallelSorting(&buffer2);
+    statuspar = SortingCheck(&buffer2);
+
+    
+    ASSERT_EQ(buffer2, buffer1);
+}
+
+// TEST(Seqvssspar, Test_Only_Positive) {
+//     const int length = 150000000;
+//     int statusseq = 0;
+//     int statuspar = 0;
+//     std::vector<int> buffer1(length);
+//     statusseq = generateRandomArray(&buffer1, -10000, 10000);
+//     std::vector<int> buffer2 = buffer1;
+//     double t1_seq = omp_get_wtime();
+//     statusseq = RadixSort(&buffer1);
+//     statusseq = SortingCheck(&buffer1);
+//     double t2_seq = omp_get_wtime();
+//     double time_seq = t2_seq - t1_seq;
+//     std::cout << "Time seq: " << time_seq << std::endl;
+//     double t1_omp = omp_get_wtime();
+//     statuspar = ParallelSorting(&buffer2);
+//     statuspar = SortingCheck(&buffer2);
+//     double t2_omp = omp_get_wtime();
+//     double time_omp = t2_omp - t1_omp;
+//     std::cout << "Time omp: " << time_omp << std::endl;
+//     std::cout << "Acceleration: " << time_seq / time_omp << std::endl;
+    
+    
+//     ASSERT_EQ(buffer2, buffer1);
+// }
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
