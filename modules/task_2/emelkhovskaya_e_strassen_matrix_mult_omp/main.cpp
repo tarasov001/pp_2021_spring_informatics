f6c954394394d323863f9f96d02055228487dcb9
emelkhovsky <emelkhovsky4@mail.ru>
2021-04-17 22:47:40 +0300

    third

--- modules/task_2/emelkhovskaya_e_strassen_matrix_mult_omp/main.cpp
+++ modules/task_2/emelkhovskaya_e_strassen_matrix_mult_omp/main.cpp
@@ -38,18 +38,20 @@ TEST(Sequential, matrix_128) {
     ASSERT_EQ(real_c, c);
 }
 
-TEST(Sequential, matrix_65_not_2) {
-    int size = 65;
+TEST(Sequential, matrix_32) {
+    int size = 32;
     std::vector<double> a(size * size);
     std::vector<double> b(size * size);
     std::vector<double> c(size * size);
+    std::vector<double> real_c(size * size);
 
     for (unsigned int i = 0; i < size * size; i++) {
         a[i] = i + 1;
         b[i] = size * size - i;
     }
-    ASSERT_THROW(StrassenFunc(a.data(), b.data(), c.data(), size),
-    std::invalid_argument);
+    StrassenFunc(a.data(), b.data(), c.data(), size);
+    seqMult(a.data(), b.data(), real_c.data(), size);
+    ASSERT_EQ(real_c, c);
 }
 
 TEST(Sequential, small_matrix_5_not_2) {
