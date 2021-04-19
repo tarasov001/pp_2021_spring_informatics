05b2cfc6b7e4c8cfc9dccbd99cc4ebdc9a476cdd
Leroyku <mark.kumbrasev@gmail.com>
2021-04-19 18:02:11 +0300

    f

---
+++ modules/task_2/kumbrasev_m_conjugate_gradient_omp/main.cpp
@@ -0,0 +1,72 @@
+// Copyright 2021 Kumbrasev Mark
+#include <gtest/gtest.h>
+#include "./conjugate_gradient.h"
+
+TEST(Conjugate_gradient_omp, M2) {
+    std::vector<std::vector<double>> M = { { 5, 2}, {2, 1} };
+    std::vector<double> B = { 7, 9 };
+
+    std::vector<double> X = ConjugateGradient(M, B);
+    double time1 = omp_get_wtime();
+    std::vector<double> Mx = mMultv(M, X);
+    double time2 = omp_get_wtime();
+    std::cout << time2 - time1 << std::endl;
+    std::vector<double> res = { { 7, 9 } };
+    for (size_t i = 0; i < res.size(); ++i) {
+        ASSERT_NEAR(Mx[i], res[i], 0.5);
+    }
+}
+TEST(Conjugate_gradient_omp, X) {
+    std::vector<std::vector<double>> M = { { 5, 2}, {2, 1} };
+    std::vector<double> B = { 7, 9 };
+
+    std::vector<double> X = ConjugateGradient(M, B);
+    std::vector<double> res = { { -11, 31 } };
+    for (size_t i = 0; i < res.size(); ++i) {
+        ASSERT_NEAR(res[i], X[i], 0.5);
+    }
+}
+TEST(Conjugate_gradient_omp, M3) {
+    std::vector<std::vector<double>> M = { { 1, 5, 3 }, {2, 1, -1 }, { 4, 2, 1 } };
+    std::vector<double> B = { 31, 29, 10 };
+
+    std::vector<double> X = ConjugateGradient(M, B);
+    double time1 = omp_get_wtime();
+    std::vector<double> res = mMultv(M, X);
+    double time2 = omp_get_wtime();
+    std::cout << time2 - time1 << std::endl;
+    std::vector<double> finalr = { { 172.2, 137.814, 134.175 } };
+    for (size_t i = 0; i < res.size(); ++i) {
+        ASSERT_NE(B[i], res[i]);
+    }
+}
+TEST(Conjugate_gradient_omp, M4) {
+    std::vector<std::vector<double>> M = { { 1, -1, 3, 1 }, {4, -1, 5, 4 }, { 2, -2, 4, 1 }, { 1, -4, 5, -1 } };
+    std::vector<double> B = { 5, 4, 6, 3 };
+
+    std::vector<double> X = ConjugateGradient(M, B);
+    double time1 = omp_get_wtime();
+    std::vector<double> res = mMultv(M, X);
+    double time2 = omp_get_wtime();
+    std::cout << time2 - time1 << std::endl;
+    for (size_t i = 0; i < res.size(); ++i) {
+        ASSERT_NE(B[i], res[i]);
+    }
+}
+TEST(Conjugate_gradient_omp, MultiMB) {
+    std::vector<std::vector<double>> M = { { 2, 6, 1 }, { 3, 1, 7 }, { 8, 9, 1 } };
+    std::vector<double> B = { 7, 4, 1 };
+
+    double time1 = omp_get_wtime();
+    std::vector<double> res = mMultv(M, B);
+    double time2 = omp_get_wtime();
+    std::cout << time2 - time1 << std::endl;
+    std::vector<double> I = { { 39, 32, 93 } };
+    for (size_t i = 0; i < res.size(); ++i) {
+        ASSERT_EQ(I[i], res[i]);
+    }
+}
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
