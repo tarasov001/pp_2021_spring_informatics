d3653614b2a39e62943cb70b2bc6239c1369683f
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2021-04-21 22:32:22 +0300

    Update labeling.cpp

--- modules/task_2/paranicheva_a_omplabeling/labeling.cpp
+++ modules/task_2/paranicheva_a_omplabeling/labeling.cpp
@@ -1,10 +1,9 @@
 // Copyright 2021 Paranicheva Alyona
-#include <omp.h>
 #include <iostream>
 #include <random>
 #include <ctime>
 #include <vector>
-#include <utility>
+#include <algorithm>
 #include "../../../modules/task_2/paranicheva_a_omplabeling/labeling.h"
 
 std::vector<int> getRandomMatrix(int rows, int cols) {
