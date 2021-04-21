f7f7d16a785c0891cfad4f55bb5f7673ab5536b6
ParanichevaAlyona <43447194+ParanichevaAlyona@users.noreply.github.com>
2021-04-21 23:15:21 +0300

    Update labeling.h

--- modules/task_2/paranicheva_a_omplabeling/labeling.h
+++ modules/task_2/paranicheva_a_omplabeling/labeling.h
@@ -13,5 +13,6 @@ std::vector<int> CLabeling(const std::vector<int>&, int, int);
 
 std::pair<std::vector<int>, std::vector<int> > firstMarkOmp(std::vector<int>, int, int);
 std::vector<int> CLabelingOmp(const std::vector<int>& array, int rows, int cols);
+std::vector<int> Transform(const std::vector<int>& array, int rows, int cols);
 
 #endif  // MODULES_TASK_2_PARANICHEVA_A_OMPLABELING_LABELING_H_
