0903f21dac0cead54a00c229b16914d0e80c54fc
Makarov-AA <43129398+Makarov-AA@users.noreply.github.com>
2021-04-26 02:03:47 +0300

    Fix linter issuses

--- modules/task_2/makarov_a_image_convex_hull/image_convex_hull.h
+++ modules/task_2/makarov_a_image_convex_hull/image_convex_hull.h
@@ -1,6 +1,6 @@
 // Copyright 2021 Makarov Alexander
-#ifndef MODULES_TASK_1_MAKAROV_A_IMAGE_CONVEX_HULL_IMAGE_CONVEX_HULL_H_
-#define MODULES_TASK_1_MAKAROV_A_IMAGE_CONVEX_HULL_IMAGE_CONVEX_HULL_H_
+#ifndef MODULES_TASK_2_MAKAROV_A_IMAGE_CONVEX_HULL_IMAGE_CONVEX_HULL_H_
+#define MODULES_TASK_2_MAKAROV_A_IMAGE_CONVEX_HULL_IMAGE_CONVEX_HULL_H_
 
 #include <vector>
 #include <list>
@@ -10,8 +10,8 @@ std::vector<int> mark_components(const std::vector<int>& bin_image, int w,
                                  int h);
 std::vector<std::list <std::pair<int, int> > > get_convex_hulls(
                            const std::vector<int>& marked_image, int w, int h);
-						   
+
 std::vector<std::list <std::pair<int, int> > > get_convex_hulls_seq(
                           const std::vector<int>& marked_image, int w, int h);
 
-#endif  // MODULES_TASK_1_MAKAROV_A_IMAGE_CONVEX_HULL_IMAGE_CONVEX_HULL_H_
+#endif  // MODULES_TASK_2_MAKAROV_A_IMAGE_CONVEX_HULL_IMAGE_CONVEX_HULL_H_
